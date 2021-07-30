5924f07a18378947c48f1caab65a1b9114753f64
Trent W. Buck <trentbuck@gmail.com>
2021-07-30 21:00:37 +1000

    Apply security patches to in-CPU x86-64 emulators

--- A-main.py
+++ A-main.py
@@ -16,7 +16,7 @@ it can run entirely without root privileges.
 It emits a kernel, ramdisk, and filesystem.squashfs.
 
 NOTE: this is the simplest config possible.
-      It lacks CRITICAL SECURITY AND DATA LOSS packages, such as amd64-microcode and smartd.
+      It lacks CRITICAL SECURITY AND DATA LOSS packages, such as smartd.
 """
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -37,6 +37,16 @@ subprocess.check_call(
      '--include=dbus',          # https://bugs.debian.org/814758
      '--include=live-config iproute2 keyboard-configuration locales sudo user-setup',
      '--include=ifupdown isc-dhcp-client',  # live-config doesn't support systemd-networkd yet.
+     # Unlike modern architectures, x86-64 does not exist in hardware.
+     # It is implemented as a non-free emulator running on undocumented RISC CPUs.
+     # Security updates cannot be installed persistently; each boot must re-install them.
+     # By default amd64-microcode and intel-microcode auto-detect the current CPU, and only install that one.
+     # This is no good for a Debian Live image; make sure security updates for ALL CPUs are included.
+     '--components=main contrib non-free',
+     '--include=intel-microcode amd64-microcode iucode-tool',
+     '--essential-hook=>$1/etc/default/intel-microcode echo IUCODE_TOOL_INITRAMFS=yes IUCODE_TOOL_SCANCPUS=no',
+     '--essential-hook=>$1/etc/default/amd64-microcode echo AMD64UCODE_INITRAMFS=yes',
+     '--dpkgopt=force-confold',
      'bullseye',
      args.output_file])
 
