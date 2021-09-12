944f9c051b74301a2ababc899415fc1c3dbd3448
Trent W. Buck <trentbuck@gmail.com>
2021-09-13 05:29:46 +1000

    Use NFSv4 sec=sys (not NFSv3) in initrd

--- debian-11-main.py
+++ debian-11-main.py
@@ -90,7 +90,8 @@ subprocess.check_call(
         '--essential-hook=tar-in debian-11-main.tar /',
         '--customize-hook=systemctl --root=$1 enable systemd-networkd']
        if args.optimize != 'simplicity' else []),
-     *(['--essential-hook=tar-in debian-11-main.netboot.tar /']  # 9% faster 19% smaller
+     *(['--include=nfs-common',  # for zz-nfs4 (see tarball)
+        '--essential-hook=tar-in debian-11-main.netboot.tar /']  # 9% faster 19% smaller
        if args.netboot else []),
      *(['--customize-hook=echo root: | chroot $1 chpasswd --crypt-method=NONE']
        if args.backdoor_enable else []),
