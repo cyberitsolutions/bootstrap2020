2e3c39e106ef9b4d706ce8a3a3bce525a2f9bf32
Trent W. Buck <trentbuck@gmail.com>
2021-10-13 08:12:36 +1100

    backdoor: add non-root user "x" with password "x"
    chromium, pipewire, and xdm all block "root" user.
    (xdm also blocks passwordless users.)
    
    When doing GUI testing, I got sick of switching to ssh and typing
        adduser x RET x RET x RET RET RET RET ^D
    over and over and over.
    
    NOTE: can't combine the two chpasswd calls, because
          this says "on login don't even ask for a password" (what we want):
              echo root: | chpasswd --crypt-method=NONE
          whereas this says "there is a password, prompt for it, it is ''"
              echo root: | chpasswd
          and this is not allowed due to restrictions in /etc/pam.d/:
              echo x:x | chpasswd --crypt-method=NONE

--- debian-11-main.py
+++ debian-11-main.py
@@ -359,7 +359,9 @@ with tempfile.TemporaryDirectory() as td:
             '--customize-hook=rm -fv $1/etc/ssh/ssh_host_*_key*',
             '--customize-hook=systemctl --root=$1 enable bootstrap2020-openssh-keygen']
            if args.optimize != 'simplicity' else []),
-         *(['--customize-hook=echo root: | chroot $1 chpasswd --crypt-method=NONE']
+         *(['--customize-hook=chroot $1 adduser x --gecos x --disabled-password',
+            '--customize-hook=echo x:x | chroot $1 chpasswd',
+            '--customize-hook=echo root: | chroot $1 chpasswd --crypt-method=NONE']
            if args.backdoor_enable else []),
          *([f'--customize-hook=echo bootstrap:{git_description} >$1/etc/debian_chroot',
             '--customize-hook=chroot $1 bash -i',
