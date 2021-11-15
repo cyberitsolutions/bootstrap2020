2ab8a226b9d5bee811f9c1cd86ba8dc8af85eb73
Trent W. Buck <trentbuck@gmail.com>
2021-11-15 19:58:49 +1100

    inmate: set login theme colors

---
+++ debian-11-PrisonPC/xdm/xdm-pre-prompt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import subprocess
+
+__doc__ == """
+This script runs AS ROOT after X starts
+but before the xdm login prompt is displayed.
+"""
+
+
+# Xorg supports -br ("black background", the default),
+#               -wr ("white background"), and
+#               -retro (stipple background).
+# It doesn't support anything else.
+# Therefore we have to set the background by hand.
+# We can either ask query the root window, or
+# we can parse /etc/X11/xdm/Xresources, or
+# we can try to use a file both xdm and python can read.
+# For now, query the root window.
+stdout = subprocess.check_output(['xrdb', '-query'], text=True)
+xresources = dict(line.split(':\t', 1) for line in stdout.splitlines())
+background_color = xresources['xlogin.Login.Background']
+subprocess.check_call(['xsetroot', '-solid', background_color])
