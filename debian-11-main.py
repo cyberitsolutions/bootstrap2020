66d0fd3af4b259f2de4545c375dd6438f0c9314b
Trent W. Buck <trentbuck@gmail.com>
2021-12-01 20:09:40 +1100

    inmate: log terminal attempts
    The real purpose of this is actually to stop XFCE saying
    "no terminals installed... run an arbitrary command instead?"
    It's fairly harmless but it looks unprofessional.
    
    https://alloc.cyber.com.au/task/task.php?taskID=30241

--- debian-11-main.py
+++ debian-11-main.py
@@ -378,6 +378,7 @@ with tempfile.TemporaryDirectory() as td:
                ] if args.apps else []),
             *(['--include=prisonpc-bad-package-conflicts'
                '    python3-gi gir1.2-gtk-3.0'  # for acceptable-use-policy.py
+               '    gir1.2-notify-0.7'          # for log-terminal-attempt.py (et al)
                '    libgtk-3-bin'  # gtk-launch (used by some .desktop files)
                '    fonts-adf-universalis'      # UI font (FIXME: fonts-prisonpc-core later)
                '    x11vnc'  # https://en.wikipedia.org/wiki/Panopticon#Surveillance_technology
