1871afdcca200c751484ec0346d006c1425bacd4
Trent W. Buck <trentbuck@gmail.com>
2021-12-29 14:43:03 +1100

    tv-snitch: systemd integration

--- debian-11-PrisonPC/snitches/tv-snitch.py
+++ debian-11-PrisonPC/snitches/tv-snitch.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-
-# Because TV traffic is multicast,
-# it is not possible to block it by host (or user) at the source.
-# We MUST block it on the individual desktop. (#25324, #22980)
-#
-# Constraints:
-#  * if this script dies, MUST block TV, MAY force reboot.
-#  * MUST NOT have a 60s delay between logging in and TV becoming available.
-#  * needs to know the username, to check tvviewers membership.
-#  * a single transient network/pete outage MUST NOT break TV for 60s.
-#
-# This script is started from xdm, as root,
-# with the logged-in username in the environment.
-# (There's no elegant way to start it via systemd.)
-# --twb, Jan 2016
-
-import subprocess               # for iptab
-import os
-import syslog
+import argparse
 import grp
+import subprocess               # for nft
+import sys
 import time
-import urllib.request
 import urllib.error
+import urllib.request
+
+import systemd.daemon
+
+__doc__ = """ allow/block IPTV based on master server's opinion
+
+Because TV traffic is multicast,
+it is not possible to block it by host (or user) at the source.
+We MUST block it on the individual desktop. (#25324, #22980)
+
+Constraints:
+ * if this script dies, MUST block TV, MAY force reboot.
+ * MUST NOT have a 60s delay between logging in and TV becoming available.
+ * needs to know the username, to check tvviewers membership.
+ * a single transient network/pete outage MUST NOT break TV for 60s.
+
+--twb, Jan 2016
+"""
+
 
 was_drop = True                 # used to decide whether to syslog
 
@@ -64,9 +66,9 @@ def enact(drop, why=None):
     # We deliberately DO NOT use this to decide whether to run iptables,
     # because we might misremember!
     if drop and not was_drop:
-        syslog.syslog('Revoking TV access ({})'.format(why))
+        print(f'Revoking TV access ({why})', file=sys.stderr, flush=True)  # for syslog
     if was_drop and not drop:
-        syslog.syslog('Granting TV access ({})'.format(why))
+        print(f'Granting TV access ({why})', file=sys.stderr, flush=True)  # for syslog
 
     was_drop = drop             # Update our memory.
 
@@ -87,24 +89,25 @@ def pete_says_drop():           # --> True unless pete says ACCEPT.
     while attempts < 6:
         attempts += 1
         try:
-            return b'ACCEPT' != urllib.request.urlopen('https://ppc-services/tvcurfew').read()
+            with urllib.request.urlopen('https://ppc-services/tvcurfew') as f:
+                return b'ACCEPT' != f.read()
         except urllib.error.URLError as e:
-            syslog.syslog('pete_says_drop(): {}'.format(e))
+            print('<3>pete_says_drop():', e, file=sys.stderr, flush=True)  # for syslog
             time.sleep(2 ** attempts)
     return True                 # ran out of attempts, claim pete said b'DROP'
 
 
 def main():
-    user = os.getenv('USER')
-    assert user
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('user')
+    args = parser.parse_args()
 
     # FIXME: for some reason, it defaults to "/tv-snitch" not "tv-snitch".
-    syslog.openlog('tv-snitch')
-
-    syslog.syslog('Ready!')
+    systemd.daemon.notify('READY=1')  # tell systemd to start WatchdogSec= countdown
 
     # FIXME: don't log *all* results, but *do* log state *transitions*!!!
-
     while True:
         # Do an LDAP query: is the user a member of tvviewers?
         # NB: going via nss means this is cached,
@@ -114,7 +117,7 @@ def main():
         # Instead of checking this client side,
         # we should just send pete the logged-in username and let HIM decide.
         # (As at Jan 2016, nscd/unscd aren't on the master server.)
-        if user not in grp.getgrnam('tvviewers').gr_mem:
+        if args.user not in grp.getgrnam('tvviewers').gr_mem:
             enact(drop=True, why='group')
 
         # Do an HTTP query: does the server say this IP is not curfewed (for TV)?
@@ -123,6 +126,8 @@ def main():
 
         else:
             enact(drop=False)
+
+        systemd.daemon.notify('WATCHDOG=1')  # reset WatchdogSec= countdown
 
         # Wait a bit before checking again.
         time.sleep(60)
@@ -134,10 +139,10 @@ try:
 # it should never finish.  If it DOES finish,
 # something has gone drastically wrong.
 finally:
-    syslog.syslog('Something went wrong, trying to deny...')
+    print('<1>Something went wrong, trying to deny...', file=sys.stderr, flush=True)  # for syslog
     enact(drop=True, why='crash')
 ## UPDATE: with this, python exits *BEFORE THE BACKTRACE PRINTS*
 ## ARGHAR GHARGJAHEGJH!@#*&^!@*&#^!*&@#^
 #    exit(1)
 ## Putting the exit *outside* the try/finally should be OK, though.
-exit (1)
+exit(1)
