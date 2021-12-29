f66285703a49aed864b9e9f827a358641ab8881a
Trent W. Buck <trentbuck@gmail.com>
2021-12-30 08:14:55 +1100

    kernel: slurp Debian 9 config for 4.19.118-2+deb10u1~bpo9+1
    Also experimentally switch it over from a bunch of bash arrays to a .ini file

---
+++ debian-11-PrisonPC.packages/build-inmate-kernel.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+import argparse
+import configparser
+import subprocess
+
+
+__doc__ = """ build kernel with Debian patches and config, modulo PrisonPC policy
+
+From https://KB.cyber.com.au/PrisonPC+Prisoner+Kernel the high-level policy is:
+
+   =======         =====================================   ==============
+   RFC2119         COMPONENT                               EXAMPLES
+   =======         =====================================   ==============
+   MUST            USB keyboard/mouse
+   MUST            SATA AHCI CD/DVD
+   MUST            GPUs (mainstream only)
+   MUST            Sound cards (mainstream only)
+   MUST            NFSv4, SquashFS, AUFS, ISO9660, UDF
+   MUST            UTF-8
+   SHOULD          NICs (1Gbps ethernet)
+   MUST            Yama (LSM)
+
+   MUST NOT        Wireless networking                     802.11, 802.15.4, BT, WIMAX, NFC
+   MUST NOT        USB storage
+   SHOULD NOT      All other USB
+   SHOULD NOT      All other storage                       SATA, SCSI, MMC
+   MAY             All other filesystems
+   MAY             All other input devices
+   MAY             All other sound cards
+   MAY             All other networking                    10M/10G ethernet, FC
+   MAY             All other LSMs                          SELinux, Apparmor
+
+   MUST NOT        Unnecessary crypto
+   SHOULD NOT      Unnecessary serial
+
+   AS NEEDED       All other buses                         1-wire, SCADA, IDE, PCI
+   MAY             All other encodings                     CP437, ISO8859, KOI-8R
+
+   SHOULD NOT      debugging
+   =============   =====================================   ==============
+
+The process is a bit messy because we combine settings from
+conflicting sources.
+
+  1. When the kernel changes (since this last run),
+
+     a. Apply OLD debian /boot/config to CURRENT debian source.
+        Run "make syncconfig" (née "make oldconfig").
+        This will prompt for each new thing, defaulting to the MAINLINE default.
+
+     b. Compare OLD debian /boot/config and CURRENT debian /boot/config.
+        This shows the DEBIAN setting for each new thing.
+
+     c. Run "make menuconfig" and/or grep --include=Kconfig.
+        Sometimes this view is easier to understand.
+
+     d. A human decides if the answer should be
+
+        * MUST or SHOULD or SHOULD NOT or MUST NOT:
+          it goes into build-inmate-kernel.ini; or
+
+        * DON'T CARE:
+          it is not mentioned.
+
+     e. move .config-current to .config-old
+
+     f. git commit .ini and .config-old as "kernel: bump to NNN".
+
+     This is generally quick for a single release.
+     For example, 5.1 to 5.2  might have  5 new prompts.
+     For example, 5.1 to 5.10 might have 50 new prompts.
+
+  2. Once that is done, do a "real" build,
+     which should be fully automated.
+     It uploads to Cyber IT's apt repo, like build-misc.py.
+"""
+
+
+parser = configparser.ConfigParser()
+parser.read('build-inmate-kernel.ini')
+config_arguments = [
+    arg
+    for section in parser.sections()
+    for key, value_str in parser[section].items()
+    for value in value_str.split()
+    for arg in ['-d' if key in {'should not', 'must not'} else '-e',
+                value]]
+
+print('The full command is', *config_arguments)
