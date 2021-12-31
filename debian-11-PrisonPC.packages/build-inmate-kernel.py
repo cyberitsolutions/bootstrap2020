bf2590976532aa5f30963179a88cd25b38df85ac
Trent W. Buck <trentbuck@gmail.com>
2021-12-31 18:52:20 +1100

    kernel: reindent

--- debian-11-PrisonPC.packages/build-inmate-kernel.py
+++ debian-11-PrisonPC.packages/build-inmate-kernel.py
@@ -100,83 +100,83 @@ args = parser.parse_args()
 apt_proxy = subprocess.check_output(['auto-apt-proxy'], text=True).strip()
 
 with tempfile.TemporaryDirectory() as td:
-  td = pathlib.Path(td)
-  subprocess.check_call(
-    ['mmdebstrap',
-     '--variant=buildd',
-     f'--aptopt=Acquire::http::Proxy "{apt_proxy}"',
-     '--aptopt=Acquire::https::Proxy "DIRECT"',
-     '--dpkgopt=force-unsafe-io',
-     '--dpkgopt=path-exclude=/usr/share/doc/*',
+    td = pathlib.Path(td)
+    subprocess.check_call(
+        ['mmdebstrap',
+         '--variant=buildd',
+         f'--aptopt=Acquire::http::Proxy "{apt_proxy}"',
+         '--aptopt=Acquire::https::Proxy "DIRECT"',
+         '--dpkgopt=force-unsafe-io',
+         '--dpkgopt=path-exclude=/usr/share/doc/*',
 
-     # We install linux-image-amd64, but only for /boot/config.
-     # Save a little time by skipping the rest.
-     '--dpkgopt=path-exclude=/lib/modules/*',
+         # We install linux-image-amd64, but only for /boot/config.
+         # Save a little time by skipping the rest.
+         '--dpkgopt=path-exclude=/lib/modules/*',
 
-     # This include list is from Debian 9 and is probably out-of-date!
-     # '--include=build-essential devscripts curl wget bc libncurses-dev lsb-release fakeroot',
-     '--include=gcc-10-plugin-dev',  # for CONFIG_GCC_PLUGIN_*
+         # This include list is from Debian 9 and is probably out-of-date!
+         # '--include=build-essential devscripts curl wget bc libncurses-dev lsb-release fakeroot',
+         '--include=gcc-10-plugin-dev',  # for CONFIG_GCC_PLUGIN_*
 
-     # We call "apt build-dep", so this line is not strictly needed.
-     # I put it here only because mmdebstrap installs much more quietly than apt --quiet.
-     # The only downside is if upstream's build-deps change, then
-     # we'll waste a little time and space.
-     # https://sources.debian.org/src/linux/5.14.9-2%7Ebpo11+1/debian/control/#L7-L9
-     '--include='
-     ' debhelper dh-exec python3 quilt cpio xz-utils dh-python bison flex'
-     ' kernel-wedge kmod bc libssl-dev openssl libelf-dev rsync lz4'
-     ' dwarves gcc-10 python3-docutils zlib1g-dev libcap-dev libpci-dev'
-     ' autoconf automake libtool libglib2.0-dev libudev-dev libwrap0-dev'
-     ' asciidoctor gcc-multilib libaudit-dev libbabeltrace-dev'
-     ' libbabeltrace-dev libdw-dev libiberty-dev libnewt-dev libnuma-dev'
-     ' libperl-dev libunwind-dev libopencsd-dev python3-dev'
-     ' graphviz python3-sphinx python3-sphinx-rtd-theme'
-     ' texlive-latex-base texlive-latex-extra dvipng patchutils',
+         # We call "apt build-dep", so this line is not strictly needed.
+         # I put it here only because mmdebstrap installs much more quietly than apt --quiet.
+         # The only downside is if upstream's build-deps change, then
+         # we'll waste a little time and space.
+         # https://sources.debian.org/src/linux/5.14.9-2%7Ebpo11+1/debian/control/#L7-L9
+         '--include='
+         ' debhelper dh-exec python3 quilt cpio xz-utils dh-python bison flex'
+         ' kernel-wedge kmod bc libssl-dev openssl libelf-dev rsync lz4'
+         ' dwarves gcc-10 python3-docutils zlib1g-dev libcap-dev libpci-dev'
+         ' autoconf automake libtool libglib2.0-dev libudev-dev libwrap0-dev'
+         ' asciidoctor gcc-multilib libaudit-dev libbabeltrace-dev'
+         ' libbabeltrace-dev libdw-dev libiberty-dev libnewt-dev libnuma-dev'
+         ' libperl-dev libunwind-dev libopencsd-dev python3-dev'
+         ' graphviz python3-sphinx python3-sphinx-rtd-theme'
+         ' texlive-latex-base texlive-latex-extra dvipng patchutils',
 
-     # Get the /boot/config-* to be copied out as "config-current".
-     *(['--include=curl ca-certificates tiny-initramfs',
-        f'--customize-hook=chroot $1 curl --output x.deb --proxy {apt_proxy} {args.deb_url}',
-        '--customize-hook=chroot $1 apt install --assume-yes --quiet ./x.deb']
-       if args.deb_url else
-       # NORMAL USAGE: just delegate to apt.
-       ['--include=linux-image-amd64 tiny-initramfs']),
-     '--essential-hook=mkdir -p $1/etc/apt/preferences.d/',
-     '--essential-hook=copy-in ../debian-11-main/apt-preferences-bullseye-backports /etc/apt/preferences.d/',
-     '--customize-hook=cp -T $1/boot/config-* $1/boot/build-inmate-kernel.config-current',
-     '--customize-hook=copy-out boot/build-inmate-kernel.config-current ./',
-     '--customize-hook=copy-in build-inmate-kernel.config-old /boot/',
+         # Get the /boot/config-* to be copied out as "config-current".
+         *(['--include=curl ca-certificates tiny-initramfs',
+            f'--customize-hook=chroot $1 curl --output x.deb --proxy {apt_proxy} {args.deb_url}',
+            '--customize-hook=chroot $1 apt install --assume-yes --quiet ./x.deb']
+           if args.deb_url else
+           # NORMAL USAGE: just delegate to apt.
+           ['--include=linux-image-amd64 tiny-initramfs']),
+         '--essential-hook=mkdir -p $1/etc/apt/preferences.d/',
+         '--essential-hook=copy-in ../debian-11-main/apt-preferences-bullseye-backports /etc/apt/preferences.d/',
+         '--customize-hook=cp -T $1/boot/config-* $1/boot/build-inmate-kernel.config-current',
+         '--customize-hook=copy-out boot/build-inmate-kernel.config-current ./',
+         '--customize-hook=copy-in build-inmate-kernel.config-old /boot/',
 
-     *(['--include=libncurses-dev git less'] if args.menuconfig else []),
+         *(['--include=libncurses-dev git less'] if args.menuconfig else []),
 
-     # Download the source
-     *(['--include=curl ca-certificates devscripts debian-keyring',
-        f'--customize-hook=chroot $1 env http_proxy={apt_proxy} dget {args.dsc_url}']
-       if args.dsc_url else
-       # NORMAL USAGE: just delegate to apt.
-       # Do a nasty hack to turn on deb-src :-(
-       ['--customize-hook=sed -rsi "/Types:/cTypes: deb deb-src" $1/etc/apt/sources.list.d/*',
-        '--customize-hook=chroot $1 apt update --quiet',
-        '--customize-hook=chroot $1 apt source linux --quiet']),
+         # Download the source
+         *(['--include=curl ca-certificates devscripts debian-keyring',
+            f'--customize-hook=chroot $1 env http_proxy={apt_proxy} dget {args.dsc_url}']
+           if args.dsc_url else
+           # NORMAL USAGE: just delegate to apt.
+           # Do a nasty hack to turn on deb-src :-(
+           ['--customize-hook=sed -rsi "/Types:/cTypes: deb deb-src" $1/etc/apt/sources.list.d/*',
+            '--customize-hook=chroot $1 apt update --quiet',
+            '--customize-hook=chroot $1 apt source linux --quiet']),
 
-     '--include=python3',
-     '--customize-hook=copy-in build-inmate-kernel.ini /',
-     '--customize-hook=copy-in build-inmate-kernel-inner.py /',
-     f'--customize-hook=chroot $1 python3 build-inmate-kernel-inner.py {"--menuconfig" if args.menuconfig else ""} || chroot $1 bash',
+         '--include=python3',
+         '--customize-hook=copy-in build-inmate-kernel.ini /',
+         '--customize-hook=copy-in build-inmate-kernel-inner.py /',
+         f'--customize-hook=chroot $1 python3 build-inmate-kernel-inner.py {"--menuconfig" if args.menuconfig else ""} || chroot $1 bash',
 
-     # Copy the built kernel back out.
-     f'--customize-hook=sync-out /X {td}',
+         # Copy the built kernel back out.
+         f'--customize-hook=sync-out /X {td}',
 
-     'bullseye',
-     '/dev/null',
-     '../debian-11.sources'
-     ])
-  if args.upload:
-    # debsign here?
-    package_version, = [
-        path.name.split('_')[1]
-        for path in td.glob('*.changes')]
-    subprocess.check_call([
-        'rsync', '-ai', '--info=progress2', '--protect-args',
-        '--no-group',       # allow remote sgid dirs to do their thing
-        f'{td}/',     # trailing suffix forces correct rsync semantics
-        f'apt.cyber.com.au:/srv/apt/PrisonPC/pool/bullseye/desktop/linux-{package_version}/'])
+         'bullseye',
+         '/dev/null',
+         '../debian-11.sources'
+         ])
+    if args.upload:
+        # debsign here?
+        package_version, = [
+            path.name.split('_')[1]
+            for path in td.glob('*.changes')]
+        subprocess.check_call([
+            'rsync', '-ai', '--info=progress2', '--protect-args',
+            '--no-group',       # allow remote sgid dirs to do their thing
+            f'{td}/',     # trailing suffix forces correct rsync semantics
+            f'apt.cyber.com.au:/srv/apt/PrisonPC/pool/bullseye/desktop/linux-{package_version}/'])
