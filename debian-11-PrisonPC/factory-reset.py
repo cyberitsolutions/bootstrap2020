182868f57a8c968872524803fe8b5c09e2fba782
Trent W. Buck <trentbuck@gmail.com>
2022-01-11 13:15:20 +1100

    factory-reset: import as-is from Debian 9
    https://alloc.cyber.com.au/task/task.php?taskID=23679
    https://alloc.cyber.com.au/task/task.php?taskID=24922

---
+++ debian-11-PrisonPC/factory-reset.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This for the inmate user to reset there account.
+# NOTE: if you change this, also change prisonpc:reset_user_desktop_config.
+#
+# NOTE: we use os.listdir instead of glob() because ~ may contain glob
+# special characters.  Ref. https://www.python.org/sf/8402
+
+
+import errno
+import gtk
+import os.path
+import shutil
+import subprocess
+import sys
+
+# Without this, users cannot log in while over quota, due to a bug in xdm.
+# --cjb, Aug 2014
+#
+# When you log in, xdm lets your apps talk to the X server by creating
+# an MIT COOKIE in ~/.Xauthority.
+#
+# If that fails, xdm instead writes the cookie to /tmp/.XauthXXXXXX
+# and sets $XAUTHORITY so your apps can find it.
+#
+# When you're over (hard) disk quota,
+# you can create *empty* files,
+# just not write into them.
+#
+# xdm only checks for the former, so the end result is that no MIT
+# COOKIE is written, your apps aren't allowed to talk to X,
+# and your X session ends as soon as it begins.
+#
+# Creating a DIRECTORY ~/.Xauthority-c/ is enough to convince xdm that
+# it can't write the cookie to ~/.Xauthority,
+# so it falls back to /tmp/.XauthXXXXXX.
+#
+# This means you can log in while over hard quota & delete files,
+# instead of having to beg a staff member to do so from the admin
+# interface.
+#
+# The PrisonPC server creates the ~/.Xauthority-c initially,
+# we just need to be careful not to delete it here.
+# --twb, Sep 2016
+#
+# Ref. http://sources.debian.net/src/xdm/1:1.1.8-5/auth.c/#L1256
+# Ref. http://sources.debian.net/src/xdm/1:1.1.11-3/xdm/auth.c/?hl=1500#L1344
+MAGIC_XDM_DIR = '.Xauthority-c'
+
+dialog = gtk.MessageDialog(type=gtk.MESSAGE_WARNING,
+                           buttons=gtk.BUTTONS_YES_NO)
+dialog.set_title('Factory Reset')
+dialog.label.set_markup(
+    'All settings will be reset to PrisonPC defaults.\n'
+    'Regular office documents will <i>not</i> be affected.\n'
+    'You will be logged out.\n'
+    '\n'
+    '<b>Reset account to factory defaults?</b>\n')
+
+
+# NFSv3, like Windows, won't let you unlink open files.
+# Instead it renames the inode to a temporary name like ./.nfsN.
+# When the last file handle is closed, it's deleted properly.
+#
+# This means we can't simple use "rm -rf",
+# because it'll abort on the first open file handle.
+# We can't kill all those processes, because we need some.
+#
+# NOTE: I don't understand why, but omitting the "return" lines,
+# or changing them to "pass", breaks the handler.
+# Likewise "return False" instead of "raise". --twb, Sep 2016
+def handle_error(function, path, excinfo):
+    _, exception, _ = excinfo
+    if (function == os.remove
+        and isinstance(exception, OSError)
+        and exception.errno == errno.EBUSY
+        and os.path.basename(path).startswith('.nfs')):
+        print exception, 'IGNORED - looks like a stale NFS inode.'
+        return
+    if (function == os.rmdir
+        and isinstance(exception, OSError)
+        and exception.errno == errno.ENOTEMPTY):
+        print exception, 'IGNORED - parent dir of a stale NFS inode?'
+        return
+    else:
+        raise
+
+
+if gtk.RESPONSE_YES == dialog.run():
+    home = os.path.expanduser('~/')
+    assert (home.startswith('/home/'))
+    os.chdir(home)      # Easier than messing with os.path.join below.
+    for path in os.listdir('.'):  # FIXME: os.listdir() in python3.
+        if path.startswith('.'):  # Only remove hidden files!
+            if path != MAGIC_XDM_DIR:
+                # NOTE: os.walk was MUCH SLOWER than shutil.rmtree.
+                if os.path.isdir(path) and not os.path.islink(path):
+                    shutil.rmtree(path, onerror=handle_error)
+                else:
+                    # rmtree() refuses to remove symlinks and regular files,
+                    # so we have to repeat ourselves here.  SIGH.
+                    # Minimize duplication by re-using the handler.
+                    try:
+                        os.remove(path)
+                    except OSError:
+                        handle_error(os.remove, path, sys.exc_info())
+
+    # Just in case the user manually deleted it...
+    subprocess.check_call(['install', '-d', MAGIC_XDM_DIR])
+
+    # Force a logout in the simplest way:
+    # force-kill any process we're allowed to.
+    # Killing xfce4-session only might be cleaner,
+    # but might silently fail when the session start processes change.
+    # --twb, Sep 2016
+    subprocess.check_call(['pkill', '-9', '.'])
