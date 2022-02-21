7c20bc4e94b5edd0f8e1bee52117481d03f5552f
Trent W. Buck <trentbuck@gmail.com>
2022-02-21 16:20:45 +1100

    wesnoth: rewrite confusing make as python
    This allows wget2 to take all URLs at once, which dramatically improves download time.
    This fixes tar saying "unexpected garbage at end of tarball" or so.
    
    NOTE: temporarily re-adds EVERY addon, total ~2200MB!
    I used as a starting point the list from doc/31556-wesnoth-addons-1.14.txt

---
+++ debian-11-PrisonPC.packages/prisonpc-wesnoth-extras/debian/rules.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import configparser
+import os
+import pathlib
+import subprocess
+import tempfile
+
+lookup_table = configparser.ConfigParser()
+lookup_table.read('debian/addons.ini')
+addons = frozenset(
+    set(lookup_table['addons'].get('TODO', '').split()) |
+    set(lookup_table['addons'].get('PASS', '').split()))
+
+destdir = pathlib.Path('debian/prisonpc-wesnoth-addons/usr/share/games/wesnoth/1.14/data/add-ons/')
+destdir.mkdir(exist_ok=True, parents=True)
+with tempfile.TemporaryDirectory() as td:
+    td = pathlib.Path(td)
+    # Download every addon.
+    # Use wget2 so we get fast HTTP/2 parallel downloads.
+    with (td / 'URLs').open('w') as f:
+        for addon in addons:
+            # FIXME: use https (not http) when debugging is done.
+            # NOTE: https:// currently broken for gnutls (inc. wget/wget2/curl) due to
+            #           issuer DST Root CA X3,O=Digital Signature Trust Co.
+            print(f'http://files.wesnoth.org/addons/1.14/{addon}.tar.bz2', file=f)
+    subprocess.run(
+        ['wget2',
+         '--http-proxy', 'http://localhost:3142/',  # FIXME: remove when done debugging
+         '--input-file', 'URLs'],
+        check=True,
+        cwd=td)
+    # Extract every addon.
+    for addon in addons:
+        subprocess.check_call(['tar', '-C', destdir, '-xf', td / f'{addon}.tar.bz2'])
