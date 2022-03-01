babe6bf3373a2ee679c0e0df469ea550c012f26b
Trent W. Buck <trentbuck@gmail.com>
2022-03-01 18:02:13 +1100

    tvserver: use pathlib

--- debian-11-PrisonPC-tvserver/systemd/import-medium.py
+++ debian-11-PrisonPC-tvserver/systemd/import-medium.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import argparse
-import datetime
 import fcntl
-import os
 import pathlib
 import subprocess
 
@@ -11,31 +9,43 @@ import tvserver
 __doc__ = """ import a media file for use in local channels """
 
 
-# media import work directories
-DONE_ROOT='/srv/tv/iptv-queue/done/'
-TARGET_ROOT='/srv/tv/recorded/local/'
-IMPORT_LOCKFILE='/srv/tv/recorded/lock'
+IMPORT_LOCKFILE = pathlib.Path('/srv/tv/recorded/lock')
 
 
+# If the vob_path is 'Alice in Wonderland.vob',
+# create '/srv/tv/recorded/local/Alice in Wonderland.ts'.
+# NOTE: this used to add " YYYY-MM-DD" in Debian 9.
+#       The dvdrip code already adds YYYY-MM-DD HH:MM:SS, so
+#       we do not bother anymore.
+def media_import(cur, vob_path):
+    ts_path = pathlib.Path('/srv/tv/recorded/local/') / vob_path.stem
+    ts_path.parent.mkdir(parents=True, exist_ok=True)
 
-def media_import(cur, source_path):
-    source_filename = os.path.basename(source_path)
-    programme_name = "%s %s" % (os.path.splitext(source_filename)[0], datetime.datetime.today().strftime('%Y-%m-%d'))
-    done_path = os.path.join(DONE_ROOT, source_filename)
-    target_basepath = os.path.join(TARGET_ROOT, programme_name)
-    target_ts = "%s.ts" % target_basepath
-    target_aux = "%s.aux" % target_basepath
-    subprocess.check_call(['mkdir', '-p', os.path.dirname(target_ts)])  # FIXME: does this work?
-    # Don't include subtitle tracks.
-    # Do include all video tracks.
-    # Only include audio tracks with the metadata language tag set to eng
-    # FIXME: What about files that have no language metadata on any audio tracks?
-    subprocess.check_call(["ffmpeg", "-i", source_path, "-ac", "2", "-q", "4", "-sn", "-map", "v", "-map", "a:m:language:eng", target_ts])
-    subprocess.check_call(["ingests","-p","8192",target_ts])
-    duration_27mhz = int(subprocess.check_output(["lasts",target_aux]))
-    os.rename(source_path, done_path)
+    # Transcode the .vob to .ts
+    # FIXME: keep subtitles somehow?
+    subprocess.check_call(
+        ['ffmpeg', '-i', vob_path,
+         '-ac', '2',
+         '-q', '4',
+         '-sn',
+         '-map', 'v',                # video stream stays as-is
+         '-map', 'a:m:language:eng',  # audio stream is English-only
+         ts_path])
+    # Generate .aux from .ts
+    subprocess.check_call(['ingests', '-p', '8192', ts_path])
+    # Tell python how long the ts is.
+    duration_27mhz = int(subprocess.check_output(
+        ["lasts", ts_path.with_suffix('.aux')]))
+
+    # Tell the database.
+    # Site staff can than queue the ripped DVD to a local channel.
     query = "INSERT INTO local_media (media_id, path, name, duration_27mhz, expires_at) VALUES (uuid_generate_v5(uuid_ns_url(), 'file://'::text || %s), %s, %s, %s, (SELECT now() + lifetime::interval FROM local_media_lifetimes WHERE standard = 't' LIMIT 1))"
-    cur.execute(query, (target_basepath, target_basepath, programme_name, duration_27mhz))
+    cur.execute(query, (ts_path.with_suffix(''), ts_path.with_suffix(''), ts_path.name, duration_27mhz))
+
+    # Transcoding worked; remove the raw .vob.
+    # It used to be moved to /srv/tv/iptv-queue/done/.
+    # This caused prisons to rapidly run out of space.
+    vob_path.unlink()
 
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -44,7 +54,7 @@ args = parser.parse_args()
 
 try:
     with open(IMPORT_LOCKFILE, "w") as lockfile:
-        fcntl.flock(lockfile, fcntl.LOCK_EX|fcntl.LOCK_NB)
+        fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
         with tvserver.cursor() as cur:
             for path in args.paths:
                 media_import(cur, path)
