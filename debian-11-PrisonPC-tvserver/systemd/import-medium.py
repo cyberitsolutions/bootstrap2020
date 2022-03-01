8c09fd31c4813dfeb407c98170890da6e9bbbe26
Trent W. Buck <trentbuck@gmail.com>
2022-03-01 18:23:32 +1100

    tvserver: use pathlib(3python) not find(1)

--- debian-11-PrisonPC-tvserver/systemd/import-medium.py
+++ debian-11-PrisonPC-tvserver/systemd/import-medium.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+import datetime
 import errno
 import fcntl
 import logging
@@ -11,15 +12,12 @@ import tvserver
 __doc__ = """ import a media file for use in local channels """
 
 
-IMPORT_LOCKFILE = pathlib.Path('/srv/tv/recorded/lock')
-
-
 # If the vob_path is 'Alice in Wonderland.vob',
 # create '/srv/tv/recorded/local/Alice in Wonderland.ts'.
 # NOTE: this used to add " YYYY-MM-DD" in Debian 9.
 #       The dvdrip code already adds YYYY-MM-DD HH:MM:SS, so
 #       we do not bother anymore.
-def media_import(cur, vob_path):
+def media_import(vob_path):
     ts_path = pathlib.Path('/srv/tv/recorded/local/') / vob_path.stem
     ts_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -42,7 +40,8 @@ def media_import(cur, vob_path):
     # Tell the database.
     # Site staff can than queue the ripped DVD to a local channel.
     query = "INSERT INTO local_media (media_id, path, name, duration_27mhz, expires_at) VALUES (uuid_generate_v5(uuid_ns_url(), 'file://'::text || %s), %s, %s, %s, (SELECT now() + lifetime::interval FROM local_media_lifetimes WHERE standard = 't' LIMIT 1))"
-    cur.execute(query, (ts_path.with_suffix(''), ts_path.with_suffix(''), ts_path.name, duration_27mhz))
+    with tvserver.cursor() as cur:
+        cur.execute(query, (ts_path.with_suffix(''), ts_path.with_suffix(''), ts_path.name, duration_27mhz))
 
     # Transcoding worked; remove the raw .vob.
     # It used to be moved to /srv/tv/iptv-queue/done/.
@@ -50,16 +49,29 @@ def media_import(cur, vob_path):
     vob_path.unlink()
 
 
+# Equivalent to "find -mmin +2".
+# The dvdrip program does not create a file to say "I'm done ripping".
+# So as a crappy workaround, we just look for files that haven't been touched for at least two minutes.
+# This is basically assuming dvdrip will write at least one byte to vob_path every two minutes.
+# This is a reasonable assumption.
+#
+# FIXME: It will still try to rip incomplete DVDs if dvdrip stops due
+#        to power loss, or ENOSPC, or other similar edge cases.
+#        We should think of something more robust?
+def at_least_two_minutes_old(vob_path: pathlib.Path) -> bool:
+    return vob_path.stat().st_mtime < datetime.datetime.now() - datetime.timedelta(minutes=2)
+
+
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument('paths', type=pathlib.Path, nargs='+')
 args = parser.parse_args()
 
+lockfile_path = pathlib.Path('/srv/tv/recorded/lock')
 try:
-    with open(IMPORT_LOCKFILE, "w") as lockfile:
+    with lockfile_path.open('w') as lockfile:
         fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        with tvserver.cursor() as cur:
-            for path in args.paths:
-                media_import(cur, path)
+        for vob_path in pathlib.Path('/srv/tv/iptv-queue/inbound').glob('*.vob'):
+            if at_least_two_minutes_old(vob_path):
+                media_import(vob_path)
 # If file is locked, do nothing.
 # Compare C implementation:
 # https://github.com/util-linux/util-linux/blob/master/sys-utils/flock.c#L281-L287
