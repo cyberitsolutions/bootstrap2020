777f5fe818462fa3de3167ae33624473cb6300c9
Trent W. Buck <trentbuck@gmail.com>
2022-05-11 11:21:57 +1000

    Port one-liner from my .bash_history
    Also add --reproducible, though it does not fully work, it still enables some extra logging that will be useful in production.
    It is also about 10 times slower (because zstd cannot parallelize across CPUs), but we do not care.

---
+++ debian-11-production.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# FIXME: merge this "preset" and "loop" functionality into main.py
+
+import subprocess
+import datetime
+for template in (
+        'main',
+        'understudy',
+        'tvserver',
+        'desktop',
+        'desktop-staff',
+        'desktop-staff-amc',
+        'desktop-inmate-amc-library'):
+    subprocess.check_call([
+        './debian-11-main.py',
+        '--remove',
+        '--netboot-only',       # no ISO/USB
+        '--physical-only',      # no qemu
+        '--ssh=openssh-server',  # PrisonPC needs this
+        f'--reproducible={datetime.date.today()}',
+        '--upload-to', 'root@tweak', 'root@amc.prisonpc.com',
+        '--template', template])
