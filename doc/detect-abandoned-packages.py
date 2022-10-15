c65dc5255f6fd445f6e0a2263d555e4239edf053
Trent W. Buck <trentbuck@gmail.com>
2022-10-16 08:22:14 +1100

    initial draft implementation

---
+++ doc/detect-abandoned-packages.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+import csv
+import datetime
+import logging
+import pathlib
+import subprocess
+
+__documentation__ = """ detect packages with no upstream/Debian updates for 5+ years
+
+Such packages are probably full of sercurity issues and /de facto/
+abandoned, but they do not get removed from Debian until they someone
+notices, since there's no explicit FTBFS failures for packages once
+built, except maybe when a new arch is added, or a major library
+transition happens.  So packages that e.g. only use glibc can be
+apt-installable a long time after maintenance death.
+
+FIXME: this is super slow.
+
+FIXME: this requires --path-include=.../changelog.Debian* &
+       dpkg-parsechangelog (& perl).
+
+FIXME: this does not consider upstream abandonment yet! (IMPORTANT!)
+
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+
+with pathlib.Path('detect-abandoned-packages.tsv').open('w') as f:
+    tsv = csv.DictWriter(
+        f,
+        dialect=csv.excel_tab,
+        fieldnames=(
+            'Last Debian change',
+            'Source package name',
+            'Binary package name'))
+    tsv.writeheader()
+
+    for path in sorted(pathlib.Path('/usr/share/doc/').glob('*/changelog.Debian*')):
+        logging.debug(path)
+        if path.name.endswith('.amd64.gz'):  # FIXME: yuk
+            logging.debug('skipping binNMU: %s', path)
+            continue
+        if path.suffix != '.gz':
+            raise NotImplementedError(path)
+
+        # No dpkg-parsechangelog --format=json, so
+        # get each field separately -- sigh.
+        def field(f):
+            with subprocess.Popen(
+                    ['zcat', path], stdout=subprocess.PIPE) as zcat_proc:
+                return subprocess.check_output(
+                    ['dpkg-parsechangelog',
+                     '--file=-',
+                     '--show-field', f],
+                    # text=True,
+                    stdin=zcat_proc.stdout).decode().strip()
+        tsv.writerow({
+            'Binary package name': path.parent.name,
+            'Source package name': field('Source'),
+            'Last Debian change': datetime.datetime.fromtimestamp(
+                int(field('Timestamp')))})
