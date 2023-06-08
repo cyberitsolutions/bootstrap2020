7c9f988799502469394c19281ee2876b523e2293
Trent W. Buck <trentbuck@gmail.com>
2023-06-08 12:53:27 +1000

    Support >1 file -- mount the parent dir URL now, not a specific single file URL

--- curlftpfs.py
+++ curlftpfs.py
@@ -47,9 +47,9 @@ def main():
     parser = argparse.ArgumentParser(
         description='"Mount" a single HTTP URL, so it can in turn be loopback-mounted.')
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('url', type=type_url,
-                        # For debugging,
-                        default='https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-11.7.0-amd64-standard.iso')
+    parser.add_argument(
+        'url', type=type_url,
+        help='Example: https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/')
     parser.add_argument('mountpoint', type=type_mountpoint)
     args = parser.parse_args()
     if args.debug:
@@ -62,8 +62,6 @@ def main():
 
 class MyFS(fuse.Operations):
     def __init__(self, url):
-        self.url = url
-        self.filename = pathlib.Path(urllib.parse.urlsplit(url).path).name
         self.session = httpx.Client()
         # When we used requests, its built-in default "Accept-Encoding: gzip, deflate" caused problems with range requests.
         # This is because Content-Length is the compressed size.
@@ -71,31 +69,44 @@ class MyFS(fuse.Operations):
         # As a quick fix, simply disable HTTP compression.
         # https://en.wikipedia.org/wiki/HTTP_compression
         del self.session.headers['accept-encoding']
+        # Do a test request to check how stupid the server is.
         resp = self.session.head(self.url)
         resp.raise_for_status()
         if 'bytes' not in resp.headers.get('Accept-Ranges', []):
             return -errno.EOPNOTSUP  # This httpd doesn't do byte range requests
-        self.content_length = int(resp.headers['Content-Length'])
-        if self.content_length <= 0:
-            logging.warning('%s has content-length: 0?!', url)
+        if resp.http_version.startswith('HTTP/1'):
+            logging.warning('Server does not support HTTP/2? (%s)', self.url)
+        # We will use this later for .join(path).
+        self.url = resp.url
+
 
     def readdir(self, path, offset):
+        "We deny any files exist, but if you ask for them anyway, they work."
+        "The alternative is scraping startpage, DAV, index.html, or lslR.txt.gz -- or inventing our own index.json."
         logging.debug('READDIR %s', repr(path))
-        return ['.', '..', self.filename]
+        return ['.', '..']
 
     def getattr(self, path, fh=None):
         logging.debug('GETATTR %s', repr(path))
         if fh is not None:
             raise NotImplementedError()
-        if path not in {'/', f'/{self.filename}'}:
+
+        # Ask the server if the file exists, and if it does, how big it is.
+        # We don't bother to fill in anything else, because the real "meat" is inside filesystem.squashfs.
+        # We just want to server filesystem.squashfs (and maybe filesystem.module & errata.squashfs).
+        if not path.startswith('/'):
+            raise RuntimeError(path)
+        resp = self.session.head(self.url.join(path[1:]))
+        if resp.status_code == 404:
             return -errno.ENOENT
+        resp.raise_for_status()
         return {'st_mode': ((stat.S_IFDIR | 0o755) if path == '/' else (stat.S_IFREG | 0o444)),
                 'st_ino': 0,
                 'st_dev': 0,
                 'st_nlink': 2 if path == '/' else 1,
                 'st_uid': 0,
                 'st_gid': 0,
-                'st_size': 0 if path == '/' else self.content_length,
+                'st_size': int(resp.headers['Content-Length']),
                 'st_atime': 0,
                 'st_mtime': 0,
                 'st_ctime': 0}
@@ -104,23 +115,28 @@ class MyFS(fuse.Operations):
         logging.debug('OPEN %s', repr(path))
         if path == '/':
             return -errno.EISDIR
-        if path != f'/{self.filename}':
+        # We ought to error out if the URL doesn't exist.
+        if not path.startswith('/'):
+            raise RuntimeError(path)
+        resp = self.session.head(self.url.join(path[1:]))
+        if resp.status_code == 404:
             return -errno.ENOENT
+        resp.raise_for_status()
+        # Also error out if you ask for write access.
         accmode = os.O_RDONLY | os.O_WRONLY | os.O_RDWR
         if (flags & accmode) != os.O_RDONLY:
             return -errno.EACCES
         else:
-            return 0            # FIXME: UGH
+            return os.EX_OK
 
     def read(self, path, size, offset, fh=None):
         logging.debug('READ %s size=%s offset=%s fh=%s', repr(path), size, offset, fh)
         if path == '/':
             return -errno.EISDIR
-        if path != f'/{self.filename}':
-            return -errno.ENOENT
-        resp = self.session.get(
-            self.url,
-            headers={'Range': f'bytes={offset:d}-{offset + size - 1:d}'})
+        if not path.startswith('/'):
+            raise RuntimeError(path)
+        resp = self.session.get(self.url.join(path[1:]), headers={
+            'Range': f'bytes={offset:d}-{offset + size - 1:d}'})
         resp.raise_for_status()
         if len(resp.content) != size:
             logging.warning('Asked for %s bytes but got %s bytes?!', size, len(resp.content))
