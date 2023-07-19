85b94f9ca57a0103419479c4de69fd8d2c65a30a
Trent W. Buck <trentbuck@gmail.com>
2023-07-19 18:30:57 +1000

    Move template_wants_PrisonPC stuff into toml

---
+++ debian-12-PrisonPC.hooks/essential50-enable-libnss-ldapd.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import argparse
+import pathlib
+import subprocess
+
+__doc__ = """ make sure /etc/nsswitch.conf mentions ldapd """
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('chroot_path', type=pathlib.Path)
+args = parser.parse_args()
+subprocess.run(
+    ['chroot', args.chroot_path, 'debconf-set-selections'],
+    input='libnss-ldapd libnss-ldapd/nsswitch multiselect passwd group',
+    check=True,
+    text=True)
