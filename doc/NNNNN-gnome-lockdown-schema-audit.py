09a63be3eb6991d13dbd39026feca31a5ba9a9e2
Trent W. Buck <trentbuck@gmail.com>
2023-08-07 07:06:00 +1100

    Tweak the udev script scraper to do dconf schemas
    The tarball this gloms is 5MB so I am not going to commit that whole thing.

---
+++ doc/NNNNN-gnome-lockdown-schema-audit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import pathlib
+import subprocess
+import tarfile
+import tempfile
+
+
+doc = """ let me quickly see all GTK/glib/GNOME schema rules in Debian (so I can set/lock them) """
+
+with tarfile.open(name='NNNNN-gnome-lockdown-schema-audit.tar',
+                  mode='w',
+                  format=tarfile.GNU_FORMAT) as tar_handle:
+    for package_name in subprocess.check_output(
+            ['apt-file', 'search', '--package-only', '.gschema.xml'],
+            text=True).strip().splitlines():
+        with tempfile.TemporaryDirectory() as td:
+            td = pathlib.Path(td)
+            subprocess.check_call(['apt', 'download', package_name], cwd=td)
+            subprocess.check_call(['dpkg', '-x', *list(td.glob('*.deb')), '.'], cwd=td)
+            for path in sorted(list(td.glob('**/*.gschema.xml'))):
+                tar_handle.add(path, arcname=f'{package_name}/{path.relative_to(td)}')
