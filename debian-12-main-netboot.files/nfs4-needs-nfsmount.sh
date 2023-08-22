cf470c3e18c140c7a67fdede7d04208c3d131362
Trent W. Buck <trentbuck@gmail.com>
2023-08-22 14:05:57 +1000

    Document that this is still needed

--- debian-12-main-netboot.files/nfs4-needs-nfsmount.sh
+++ debian-12-main-netboot.files/nfs4-needs-nfsmount.sh
@@ -4,4 +4,5 @@
 
 # Use mount.nfs from nfs-utils (not klibc-utils nfsmount), so
 # rootfs mounts as NFSv4 (not NFSv3) [#32658]
+# NOTE: as at Debian 12 / September 2023, this is still needed.
 copy_exec /sbin/mount.nfs /bin/nfsmount
