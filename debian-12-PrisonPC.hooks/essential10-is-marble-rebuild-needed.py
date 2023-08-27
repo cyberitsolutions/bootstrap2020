8426ea5cdae987f5a6c5ba179edfbe1e09e4293a
Trent W. Buck <trentbuck@gmail.com>
2023-08-28 05:09:00 +1000

    Allow installing scratch without x-terminal-emulator
    As it is a GUI app that runs inside the Smalltalk runtime, we do not need squeak-vm shitty script to popup "which runtime did you want?", which is the only reason squeak-vm Depends: x-terminal-emulator.

--- debian-12-PrisonPC.hooks/essential10-is-marble-rebuild-needed.py
+++ debian-12-PrisonPC.hooks/essential10-is-marble-rebuild-needed.py
@@ -5,23 +5,24 @@ import subprocess
 import logging
 
 
-__doc__ = """ if Debian marble is newer than PrisonPC marble, halt and catch fire """
+__doc__ = """ if Debian X is newer than PrisonPC X, halt and catch fire """
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('chroot_path', type=pathlib.Path)
 args = parser.parse_args()
 
-policy_stdout = subprocess.check_output(
-    ['chroot', args.chroot_path,
-     'apt-cache', 'policy', 'marble'],
-    text=True)
-candidate_line, = [
-    line
-    for line in policy_stdout.splitlines()
-    if line.strip().startswith('Candidate: ')]
-if 'PrisonPC' in candidate_line:
-    logging.info("apt believes PrisonPC's marble is the newest marble")
-else:
-    logging.error("apt believes Debian's marble is newer than PrisonPC's marble -- REBUILD NEEDED!")
-    print(policy_stdout, end='', flush=True)
-    exit(1)
+for package in {'marble', 'squeak-vm'}:
+    policy_stdout = subprocess.check_output(
+        ['chroot', args.chroot_path,
+         'apt-cache', 'policy', package],
+        text=True)
+    candidate_line, = [
+        line
+        for line in policy_stdout.splitlines()
+        if line.strip().startswith('Candidate: ')]
+    if 'PrisonPC' in candidate_line:
+        logging.info(f"apt believes PrisonPC's marble is the newest {package}")
+    else:
+        logging.error(f"apt believes Debian's {package} is newer than PrisonPC's {package} -- REBUILD NEEDED!")
+        print(policy_stdout, end='', flush=True)
+        exit(1)
---
+++ debian-12-PrisonPC.packages/build-squeak-vm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import argparse
+import pathlib
+import subprocess
+import tempfile
+
+__doc__ = """ build marble without Depends: xterm | x-terminal-emulator """
+
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
+
+apt_proxy = subprocess.check_output(['auto-apt-proxy'], text=True).strip()
+
+with tempfile.TemporaryDirectory() as td:
+    td = pathlib.Path(td)
+    apt_sources_including_sources = td / 'debian-12-inc-src.sources'
+    apt_sources_including_sources.write_text(
+        pathlib.Path('../debian-12.sources').read_text().replace(
+            'Types: deb',
+            'Types: deb deb-src'))
+    subprocess.check_call(
+        ['mmdebstrap',
+         '--variant=apt',
+         f'--aptopt=Acquire::http::Proxy "{apt_proxy}"',
+         '--aptopt=Acquire::https::Proxy "DIRECT"',
+         '--dpkgopt=force-unsafe-io',
+         '--dpkgopt=path-exclude=/usr/share/doc/*',
+         '--include=build-essential,python3,devscripts',
+         '--customize-hook=chroot $1 python3 - < build-squeak-vm-inner.py',
+         f'--customize-hook=sync-out /X {td}',
+         'bookworm',
+         '/dev/null',
+         apt_sources_including_sources])
+    subprocess.check_call([
+        'rsync', '-ai', '--info=progress2', '--protect-args',
+        '--no-group',       # allow remote sgid dirs to do their thing
+        f'{td}/',     # trailing suffix forces correct rsync semantics
+        'apt.cyber.com.au:/srv/apt/PrisonPC/pool/bookworm/desktop/'])
