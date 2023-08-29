949c157cc5d8c96663073ea789a69c33912deb48
Trent W. Buck <trentbuck@gmail.com>
2023-08-29 20:37:20 +1000

    clarify that /etc/hostname will still exist at boot time due to live-boot

--- debian-12-main.hooks/customize50-dynamic-hostname.py
+++ debian-12-main.hooks/customize50-dynamic-hostname.py
@@ -2,7 +2,17 @@
 import argparse
 import pathlib
 
-__doc__ = """ Assume libnss-myhostname is installed; DO NOT use the build host's hostname """
+__doc__ = """ Assume libnss-myhostname is installed; DO NOT use the build host's hostname
+
+NOTE: When netbooting, before switch_root,
+      live-boot runs klibc-utils ipconfig (DHCPv4 client).
+
+      If the DHCP server supplies a hostname,
+      live-boot writes it to the rootfs's /etc/hostname.
+
+      This is needless, but harmless.
+      This is not easily avoidable.
+"""
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('chroot_path', type=pathlib.Path)
