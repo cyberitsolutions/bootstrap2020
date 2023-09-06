4e04d020ac3d2d8c3d832a565244e5c8ad0bbe9b
Trent W. Buck <trentbuck@gmail.com>
2023-09-06 15:27:03 +1000

    Workaround https://github.com/systemd/systemd/issues/29097

---
+++ debian-12-main.files/fix-serial-getty-terminal-type.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import logging
+import pathlib
+import re
+
+__doc__ = """ https://github.com/systemd/systemd/issues/29097 """
+
+# FIXME: deal with quoting/escaping &c in /proc/cmdline
+# FIXME: for "TERM=x TERM=y", which should win?
+# FIXME: /proc/cmdline might not be UTF-8 clean.
+cmdline = pathlib.Path('/proc/cmdline').read_text()
+try:
+    console, = re.search(r'console=(\w+)', cmdline).groups()
+    term, = re.search(r'(?:systemd.tty.term.console|TERM)=(\w+)', cmdline).groups()
+except AttributeError:
+    logging.debug('Did not find both keys; doing nothing')
+else:
+    # FIXME: systemd-escape console
+    erratum_path = pathlib.Path(
+        f'/run/systemd/generator/serial-getty@{console}.service.d/foo.conf')
+    erratum_path.parent.mkdir(exist_ok=True, parents=True)
+    erratum_path.write_text(
+        '[Service]\n'
+        f'Environment=TERM={term}\n')
