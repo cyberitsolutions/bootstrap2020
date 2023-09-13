1a8eb213f68ed14694858e553912790ed26a4f72
Trent W. Buck <trentbuck@gmail.com>
2023-09-14 02:17:07 +1000

    do MOST SOEs in a single pass

--- debian-12-production.py
+++ debian-12-production.py
@@ -3,22 +3,22 @@
 
 import subprocess
 
-for template in (
-        'understudy',
-        'tvserver',
-        'desktop-staff-amc',
-        'desktop-inmate-amc',
-        'desktop-inmate-amc-library'):
+for args in [
+        # No qemu, **EXCEPT FOR** desktop-staff-amc, which
+        # Mike wants to expose via spice-html5.
+        ['--physical-only',
+         '--templates',
+         'understudy',
+         'tvserver',
+         'desktop-inmate-amc',
+         'desktop-inmate-amc-library'],
+        ['--templates',
+         'desktop-staff-amc']]:
     subprocess.check_call([
         './debian-12-main.py',
         # Hard-code $LANG and $TZ instead of inheriting from build host.
         '--LANG', 'en_AU.UTF-8', '--TZ', 'Australia/Canberra',
         '--netboot-only',       # no ISO/USB
-        # No qemu, **EXCEPT FOR** desktop-staff-amc, which
-        # Mike wants to expose via spice-html5.
-        *(['--physical-only']
-          if template != 'desktop-staff-amc' else []),
         '--ssh=openssh-server',  # PrisonPC needs this
         '--production',
-        '--upload-to', 'root@tweak.prisonpc.com', 'root@amc.prisonpc.com',
-        '--template', template])
+        '--upload-to', 'root@tweak.prisonpc.com', 'root@amc.prisonpc.com'])
