cd53e297c140562a5fa8f5737ef8431e98b6211d
Trent W. Buck <trentbuck@gmail.com>
2023-09-14 02:13:59 +1000

    migration production wrapper as-is

---
+++ debian-12-production.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# FIXME: merge this "preset" and "loop" functionality into main.py
+
+import subprocess
+import datetime
+for template in (
+        'understudy',
+        'tvserver',
+        'desktop-staff-amc',
+        'desktop-inmate-amc',
+        'desktop-inmate-amc-library'):
+    subprocess.check_call([
+        './debian-11-main.py',
+        '--remove',
+        # Hard-code $LANG and $TZ instead of inheriting from build host.
+        '--LANG', 'en_AU.UTF-8', '--TZ', 'Australia/Canberra',
+        '--netboot-only',       # no ISO/USB
+        # No qemu, **EXCEPT FOR** desktop-staff-amc, which
+        # Mike wants to expose via spice-html5.
+        *(['--physical-only']
+          if template != 'desktop-staff-amc' else []),
+        '--ssh=openssh-server',  # PrisonPC needs this
+        f'--reproducible={datetime.date.today()}',
+        '--upload-to', 'root@tweak.prisonpc.com', 'root@amc.prisonpc.com',
+        '--template', template])
