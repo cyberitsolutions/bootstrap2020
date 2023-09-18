210fdd9118d1882c7966ff1f44bbcdddaf703d90
Trent W. Buck <trentbuck@gmail.com>
2023-09-18 21:05:08 +1000

    fixup! Make --boot-test use iPXE on OVMF EFI (not pxelinux on legacy SeaBIOS)

--- debian-12-main.py
+++ debian-12-main.py
@@ -314,7 +314,18 @@ def do_boot_test():
             ipxe_boot_args = ' '.join(
                 ['boot=live',
                  'netboot=cifs', 'nfsopts=ro,guest,vers=3.1.1',
-                 f'nfsroot=//{smb_address}/qemu live-media-path=']
+                 f'nfsroot=//{smb_address}/qemu live-media-path=',
+                 # Tell initrd and rootfs "use this NIC, don't retry *all* NICs".
+                 # https://wiki.syslinux.org/wiki/index.php?title=SYSLINUX#SYSAPPEND_bitmask
+                 # https://git.kernel.org/pub/scm/libs/klibc/klibc.git/tree/usr/kinit/ipconfig
+                 # https://salsa.debian.org/live-team/live-boot/-/blob/debian/1%2520230131/components/9990-networking.sh?ref_type=tags#L5-53
+                 # https://github.com/cyberitsolutions/bootstrap2020/blob/twb/debian-12-main.files/bootstrap2020-systemd-networkd
+                 # https://git.cyber.com.au/prisonpc/blob/4e5fd5ef09cd49e9bcb74de50afb65d61079d75e/prisonpc/tcb.py
+                 # https://github.com/systemd/systemd/blob/v254/src/network/generator/network-generator.c#L18-L44
+                 # https://ipxe.org/cfg/mac
+                 # https://ipxe.org/cmd/ifconf
+                 # NOTE: ${mac} is expanded by ipxe.efi, not us.
+                 'BOOTIF=01-${mac}']
                 if have_smbd else
                 ['boot=live',
                  f'fetch=tftp://{tftp_address}/filesystem.squashfs'])
