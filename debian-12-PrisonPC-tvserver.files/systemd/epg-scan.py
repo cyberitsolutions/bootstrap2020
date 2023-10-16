fde9f7ec7456b7d067686491aa0b1cfcafae4cb1
Trent W. Buck <trentbuck@gmail.com>
2023-10-16 18:50:12 +1100

    IPTV Debian 12: use dvblastctl (not tv-grab-dvb)
    Copied over as-is from Debian 11 tree.
    
    Not tested on Debian 12, because
    proprietary TBS drivers are broken with Linux 6.1+
    (inc. all kernels in Debian 12.)

--- debian-12-PrisonPC-tvserver.files/systemd/epg-scan.py
+++ debian-12-PrisonPC-tvserver.files/systemd/epg-scan.py
@@ -1,32 +1,114 @@
 #!/usr/bin/python3
 import argparse
+import binascii
 import datetime
 import logging
 import pathlib
 import subprocess
-import tempfile
 
 import lxml.etree
 
 import tvserver
 
-__doc__ = """ read EPG from tv_dvb_grab XML, write EPG to pg SQL
+__doc__ = """ read EPG from dvblastctl XML, write EPG to pg SQL
 
-A row in the input XML looks like this (no newlines in subtitle though):
+The dvblast output is actually from here:
 
-  <programme channel="561.dvb.guide"
-             start="20220307050000 +1100"
-             stop="20220307060000 +1100">
-    <title lang="en">Insiders</title>
-    <sub-title lang="en">
-      Australia's leading political program returns for Election Campaign 2022.
-      David Speers and the panel bring you insights and analysis of the week in politics
-      as Australians decide who will lead the nation for the next 3 years.
-    </sub-title>
-    <crid type='item'>/NC2209V006S00</crid>
-    <crid type='series'>/NC2209V</crid>
-  </programme>
+    https://github.com/videolan/bitstream/blob/master/dvb/si/eit_print.h
+    https://github.com/videolan/bitstream/blob/master/dvb/si/nit_print.h
 
+Each datum a DESC element showing the raw data with a child element:
+If bitstream doesn't understand it, the child is UNKNOWN_DESC.
+If bitstream understands it, the child is decoded data.
+
+dvblast get_nit output looks like this:
+
+    <NIT tid="64" networkid="12827" version="15" current_next="1">
+      <DESC id="0x40" length="13" value="41⋯65">
+        <NETWORK_NAME_DESC networkname="ABC Melbourne"/>
+      </DESC>
+      <TS tsid="561" onid="4112">
+        <DESC id="0x5a" length="11" value="01⋯ff">
+          <TERRESTRIAL_DESC frequency="226500000"
+                            bandwidth="7"
+                            priority="HP"
+                            timeslicing="0"
+                            mpefec="0"
+                            constellation="64-qam"
+                            hierarchy="none"
+                            coderatehp="3/4"
+                            coderatelp="not_applicable"
+                            guard="1/16"
+                            transmission="8k"
+                            otherfrequency="0" />
+        </DESC>
+        <DESC id="0x41" length="48" value="02⋯0a">
+          <SERVICE_LIST_DESC sid="560" type="0x01" />
+          <SERVICE_LIST_DESC sid="561" type="0x01" />
+          <SERVICE_LIST_DESC sid="562" type="0x01" />
+          <SERVICE_LIST_DESC sid="563" type="0x01" />
+          <SERVICE_LIST_DESC sid="564" type="0x01" />
+          <SERVICE_LIST_DESC sid="565" type="0x19" />
+          <SERVICE_LIST_DESC sid="566" type="0x02" />
+          <SERVICE_LIST_DESC sid="567" type="0x02" />
+          <SERVICE_LIST_DESC sid="568" type="0x02" />
+          <SERVICE_LIST_DESC sid="569" type="0x02" />
+          <SERVICE_LIST_DESC sid="570" type="0x02" />
+          <SERVICE_LIST_DESC sid="571" type="0x02" />
+          <SERVICE_LIST_DESC sid="572" type="0x02" />
+          <SERVICE_LIST_DESC sid="573" type="0x02" />
+          <SERVICE_LIST_DESC sid="574" type="0x02" />
+          <SERVICE_LIST_DESC sid="575" type="0x0a" />
+        </DESC>
+        <DESC id="0x5f" length="4" value="00⋯01">
+          <PRIVATE_DATA_SPECIFIER_DESC specifier="0x00003201" />
+        </DESC>
+        <DESC id="0x83" length="64" value="02⋯cc">
+          <UNKNOWN_DESC />
+        </DESC>
+      </TS>
+    </NIT>
+
+dvblast get_eit_schedule 560 output looks like this (each EVENT is one psql row):
+A row in the input XML looks like this:
+
+    <EIT tableid="0x50"
+         type="actual_schedule"
+         service_id="560"
+         version="27"
+         current_next="1"
+         tsid="561"
+         onid="4112">
+
+      <EVENT id="27019"
+             start_time="1698098400"
+             start_time_dec="2023-10-23 22:00:00 UTC"
+             duration="10800"
+             duration_dec="03:00:00"
+             running="0"
+             free_CA="0">
+        <DESC id="0x4d" length="240" value="⋯">
+          <SHORT_EVENT_DESC lang="eng"
+                            event_name="ABC News Mornings"
+                            text="When big stories break, ⋯ your stories."/>
+        </DESC>
+        <DESC id="0x54" length="2" value="2000">
+          <CONTENT_DESC content_l1="2" content_l2="0" user="0"/>
+        </DESC>
+        <DESC id="0x55" length="4" value="41555300">
+          <PARENTAL_RATING_DESC country_code="AUS"
+                                rating="0"
+                                rating_txt="undefined"/>
+        </DESC>
+        <DESC id="0x76" length="16" value="c40e2f4e553233313248323132533030">
+          <UNKNOWN_DESC />
+        </DESC>
+        <DESC id="0x76" length="10" value="c8082f4e553233313248">
+          <UNKNOWN_DESC />
+        </DESC>
+      </EVENT>
+
+    </EIT>
 
 A row in the output database looks like this:
 
@@ -71,78 +153,96 @@ I think this one-time cost is worth the trade-off of keeping the new code simple
 """
 
 
-# This function is a lo-fi alternative to
-#   re.fullmatch('prefix(.*)suffix', string).group(1)
-# or repeating a magic string twice.
-def strip(string, *, prefix='', suffix=''):
-    if not string.startswith(prefix):
-        raise RuntimeError('String did not start with prefix', string, prefix)
-    if not string.endswith(suffix):
-        raise RuntimeError('String did not end with suffix', string, suffix)
-    return string[len(prefix):-len(suffix)]
+def dvblastctl(*cmd):
+    return lxml.etree.fromstring(
+        subprocess.check_output(
+            ['dvblastctl',
+             '--remote-socket', f'/run/dvblast-{args.adapter}.sock',
+             '--print', 'xml',
+             *cmd]))
 
 
-# lxml.etree.parse().xpath() does not have an easy way to say
-# "get the first match, but error out if there are multiple matches".
-# So just roll a short version for ourselves.
-def exactly_one(matches: list):
-    if len(matches) != 1:
-        raise RuntimeError('An xpath should have returned exactly one match, but we got this...', matches)
-    return matches[0]
+def exactly_one(xs):
+    """Like xs[0], but error when len(xs) ≠ 1"""
+    x, = xs
+    return x
 
 
-def one_or_fallback(matches: list, fallback_value):
-    if len(matches) > 1:
-        raise RuntimeError('An xpath should have returned one match or no matches, but we got this...', matches)
-    return matches[0] if matches else fallback_value
+# libbitstream-dev doesn't parse CRIDs, so do it ourselves.
+# https://github.com/ostryck/tv_grab_dvb/blob/master/tv_grab_dvb.c#L433C1-L476
+# https://github.com/ostryck/tv_grab_dvb/blob/master/dvb_info_tables.c#L304-L312
+# https://ia800900.us.archive.org/32/items/etsi_ts_102_323_v01.01.01/ts_102323v010101p.pdf#page=86
+#
+# If the first byte is "c4" it's an item CRID like "crid://seven.net.au/quGsuMqSmdaPmtex"
+# If the first byte is "c8" it's a series CRID like "crid://seven.net.au/quGsuMrb"
+# If the second byte is "00", the CRID lives in some weird place (in which case, fallback).
+# If the xpath doesn't match at all, xs is falsey, and we return the fallback.
+#
+# For historical reasons, the fallback CRIDs are synthesized from the title and programme start time.
+# This should return reasonable-ish results for recurring programmes, e.g.
+#
+#     crid_item = crid://PrisonPC/2023-10-14 18:00:00 +11:00/ABC Nightly News
+#     crid_series = crid://PrisonPC/ABC Nightly News
+def parse_crid(event_obj, kind, fallback_value):
+    magic = {'item': 'c4', 'series': 'c8'}[kind]
+    if values := event_obj.xpath(f'./DESC[@id="0x76"]/@value[starts-with(., "{magic}")][not(starts-with(., "{magic}00"))]'):
+        return binascii.unhexlify(exactly_one(values))[2:]
+    else:
+        return fallback_value
 
 
-# NOTE: this comes from tv_grab_dvb.c:666:16 (parseEIT function).
-tv_grab_dvb_timestamp_format = '%Y%m%d%H%M%S %z'
+def get_programmes(obj):
+    programmes = []             # ACCUMULATOR
+    for eit_obj in obj.xpath('/EIT'):
+        service_id = int(eit_obj.get('service_id'))
+        for event_obj in obj.xpath('./EVENT'):
+            start = datetime.datetime.fromisoformat(
+                event_obj.get('start_time_dec')
+                .replace(' UTC', '+00:00'))  # Appease fromisoformat
+            duration = datetime.timedelta(seconds=int(
+                event_obj.get('duration')))
+            title = exactly_one(
+                event_obj.xpath('./DESC/SHORT_EVENT_DESC/@event_name'))
+            programmes.append({
+                'channel': None,    # not used anymore
+                'sid': service_id,
+                'start': start,
+                'stop': start + duration,
+                'title': title,
+                'sub_title': exactly_one(
+                    event_obj.xpath('./DESC/SHORT_EVENT_DESC/@text')),
+                'crid_series': parse_crid(
+                    event_obj, kind='series',
+                    fallback_value=f'crid://PrisonPC/{title}'),
+                'crid_item': parse_crid(
+                    event_obj, kind='item',
+                    fallback_value=f'crid://PrisonPC/{start}/{title}')})
+    return programmes
 
 
 parser = argparse.ArgumentParser(description=__doc__)
 group = parser.add_mutually_exclusive_group(required=True)
+# FIXME: replace this with "--remote-socket" or "--station ABC"?
 group.add_argument('--adapter', type=int)
 group.add_argument('--xml-file', type=pathlib.Path)
 args = parser.parse_args()
 
 if args.xml_file:
-    obj = lxml.etree.parse(str(args.xml_file))
+    programmes = get_programmes(lxml.etree.parse(str(args.xml_file)))
 else:
-    with tempfile.TemporaryDirectory() as td:
-        # tv_grab_dvb emits a harmless warning if "channels.conf" is missing:
-        #     No [cst]zap channels.conf to produce channel info
-        # Create an empty one so it does not complain.
-        (pathlib.Path(td) / 'channels.conf').write_text('')
-        # Run tv_grab_dvb directly, ourselves.
-        obj = lxml.etree.fromstring(subprocess.check_output(
-            ['tv_grab_dvb', '-st30', '-eISO-8859-1',
-             f'-i/dev/dvb/adapter{args.adapter}/demux0'],
-            cwd=td))
-
-programmes = []         # accumulator (so we can cursor.executemany())
-
-for programme in obj.xpath('/tv/programme'):
-    # These first two values are separate because
-    # they are referred two twice.
-    start = datetime.datetime.strptime(programme.get('start'), tv_grab_dvb_timestamp_format)
-    title = exactly_one(programme.xpath('./title[@lang="en"]/text()'))
-    programmes.append({
-        'channel': programme.get('channel'),
-        'sid': int(strip(programme.get('channel'), suffix='.dvb.guide')),
-        'start': start,
-        'stop': datetime.datetime.strptime(programme.get('stop'), tv_grab_dvb_timestamp_format),
-        'title': title,
-        'sub_title': one_or_fallback(
-            programme.xpath('./sub-title[@lang="en"]/text()'),
-            fallback_value=''),
-        'crid_series': one_or_fallback(
-            programme.xpath('./crid[@type="series"]/text()'),
-            fallback_value=f'crid://PrisonPC/{title}'),
-        'crid_item': one_or_fallback(
-            programme.xpath('./crid[@type="item"]/text()'),
-            fallback_value=f'crid://PrisonPC/{start}/{title}')})
+    # NOTE: "Service ID" is what humans think of as a "channel", e.g. "ABC News HD".
+    service_ids = dvblastctl('get_nit').xpath('/NIT/TS/DESC/SERVICE_LIST_DESC/@sid')
+    if not service_ids:
+        raise RuntimeError('No service IDs found?')
+    programmes = []
+    for service_id in service_ids:
+        try:
+            programmes += get_programmes(dvblastctl('get_eit_schedule', service_id))
+        except subprocess.CalledProcessError as e:
+            # SBS Melbourne's SID 805 and SID 806 have no EPG data.
+            # Downgrade this from "halt and catch fire" to "whinge".
+            # Otherwise we would lose EPG for *all* of SBS.
+            logging.warning('%s', e)
 
 with tvserver.cursor() as cur:
 
