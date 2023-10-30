99cd5fa70d0e2944d056944adbdb9e761369ea82
Trent W. Buck <trentbuck@gmail.com>
2023-10-30 18:06:29 +1100

    Got as far as http://localhost:8000/stations/613500000/channels/785/programmes -> big old list of programmes on SBS ONE

--- debian-11-main/POC-fastapi/myapp/__init__.py
+++ debian-11-main/POC-fastapi/myapp/__init__.py
@@ -1,3 +1,5 @@
+import contextlib
+
 import fastapi
 import psycopg2
 import psycopg2.extras
@@ -5,16 +7,61 @@ import psycopg2.extras
 app = fastapi.FastAPI()
 
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello, World!'}
+@app.get('/stations')
+async def get_stations():
+    return get_all(
+        'SELECT * FROM stations')
 
 
-@app.get('/station')
-async def get_station():
+@app.get('/stations/{frequency}')
+async def get_station(frequency: int):
+    return get_one(
+        'SELECT * FROM stations WHERE frequency = %(frequency)s',
+        {'frequency': frequency})
+
+
+@app.get('/stations/{frequency}/channels')
+async def get_channels(frequency: str):
+    return get_all(
+        'SELECT * FROM channels WHERE frequency = %(frequency)s',
+        {'frequency': frequency})
+
+
+@app.get('/stations/{frequency}/channels/{service_ID}')
+async def get_channel(frequency: str, service_ID: str):
+    return get_one(
+        'SELECT * FROM channels WHERE frequency = %(frequency)s AND sid = %(service_ID)s',
+        {'frequency': frequency,
+         'service_ID': service_ID})
+
+
+@app.get('/stations/{frequency}/channels/{service_ID}/programmes')
+async def get_programmes(frequency: str, service_ID: str):
+    return get_all(
+        'SELECT programmes.* FROM programmes JOIN channels USING (sid) WHERE channels.frequency = %(frequency)s AND channels.sid = %(service_ID)s',
+        {'frequency': frequency,
+         'service_ID': service_ID})
+
+
+@contextlib.contextmanager
+def cursor():
     with (psycopg2.connect(dbname='epg') as conn,
           conn,                 # transaction w/ rollback support
           conn.cursor(
               cursor_factory=psycopg2.extras.NamedTupleCursor) as cur):
-        cur.execute('SELECT * FROM stations')
+        yield cur
+
+
+def get_all(*args, **kwargs):
+    with cursor() as cur:
+        cur.execute(*args, **kwargs)
         return cur.fetchall()
+
+
+def get_one(*args, **kwargs):
+    with cursor() as cur:
+        cur.execute(*args, **kwargs)
+        assert cur.rowcount in {0, 1}
+        if not cur.rowcount:
+            raise fastapi.HTTPException(status_code=404)
+        return cur.fetchone()
