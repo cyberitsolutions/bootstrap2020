f82b5ef1d14eb1ea50cad76350273e483d41ea27
Trent W. Buck <trentbuck@gmail.com>
2024-01-15 11:38:51 +1100

    fixup! Staleness for all packages (not only installed packages)

--- doc/detect-abandoned-packages-all.py
+++ doc/detect-abandoned-packages-all.py
@@ -6,111 +6,88 @@ import subprocess
 # 15:35 <twb> udd=> SELECT age(max(date)), source FROM upload_history GROUP BY source ORDER BY age DESC LIMIT 10;
 # 15:37 <twb> And a sloppy limit to Debian 12 looks like: udd=> SELECT age(max(u.date)) AS age, u.source FROM upload_history u, sources s WHERE u.source = s.source AND s.release LIKE 'bookworm%' GROUP BY u.source ORDER BY age DESC LIMIT 10;
 
-subprocess.check_call([
-    'psql', 'postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd',
-    '-c', """SELECT age(max(u.date)) AS age, u.source FROM upload_history u, sources s WHERE u.source = s.source AND s.release LIKE 'bookworm%' GROUP BY u.source ORDER BY age DESC LIMIT 100;"""])
+# Update:
+#
+#   Why is cdde not reported when looking for outdated packages in bookworm?
+#
+#   https://tracker.debian.org/pkg/cdde
+#
+#       [2023-10-19] cdde 0.3.1-1.1 MIGRATED to testing (Debian testing watch)
+#       [2023-10-13] Accepted cdde 0.3.1-1.1 (source) into unstable (Bastian Germann) (signed by: bage@debian.org)
+#       [2009-02-16] cdde 0.3.1-1 MIGRATED to testing (Debian testing watch)
+#       [2008-10-25] Accepted cdde 0.3.1-1 (source i386) (Stanislav Maslovski) (signed by: Dmitry E. Oboukhov)
+#
+#   Ah because I need to JOIN USING (source, version) not just (source).
+#
+#       bash5$ ssh tweak psql postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd <<< "SELECT max(u.date), release FROM upload_history u JOIN sources s USING (source, version) WHERE u.source = 'cdde' GROUP BY 2 LIMIT 100"
+#                 max           | release
+#       ------------------------+----------
+#        2008-10-25 10:06:58+00 | bookworm
+#        2008-10-25 10:06:58+00 | bullseye
+#        2008-10-25 10:06:58+00 | buster
+#        2008-10-25 10:06:58+00 | jessie
+#        2023-10-13 19:04:46+00 | sid
+#        2008-10-25 10:06:58+00 | stretch
+#        2023-10-13 19:04:46+00 | trixie
+#       (7 rows)
+#
+#       bash5$ ssh tweak psql postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd <<< "SELECT max(u.date), release FROM upload_history u JOIN sources s USING (source) WHERE u.source = 'cdde' GROUP BY 2 LIMIT 100"
+#                 max           | release
+#       ------------------------+----------
+#        2023-10-13 19:04:46+00 | trixie
+#        2023-10-13 19:04:46+00 | stretch
+#        2023-10-13 19:04:46+00 | bullseye
+#        2023-10-13 19:04:46+00 | buster
+#        2023-10-13 19:04:46+00 | jessie
+#        2023-10-13 19:04:46+00 | bookworm
+#        2023-10-13 19:04:46+00 | sid
+#       (7 rows)
 
 
-#                 age                |            source
-# -----------------------------------+-------------------------------
-#  14 years 11 mons 8 days 13:53:02  | cdde
-#  14 years 10 mons 4 days 10:12:57  | avrp
-#  14 years 10 mons 4 days 06:12:54  | gmemusage
-#  14 years 5 mons 27 days 05:42:54  | streamripper
-#  14 years 4 mons 17 days 07:48:27  | icmptx
-#  13 years 6 mons 21 days 12:42:48  | xsettings-kde
-#  13 years 4 mons 14:20:33          | gtk2-engines-cleanice
-#  13 years 3 mons 15 days 05:27:42  | gobi-loader
-#  13 years 2 mons 13 days 21:27:53  | colortail
-#  12 years 6 mons 29 days 05:12:53  | puf
-#  12 years 6 mons 23 days 07:10:37  | libdata-streamserializer-perl
-#  12 years 6 mons 12 days 09:12:10  | rovclock
-#  12 years 5 mons 13 days 13:27:03  | dgen
-#  12 years 5 mons 2 days 07:12:40   | dynamite
-#  12 years 5 mons 01:11:55          | xkbind
-#  12 years 3 mons 11 days 02:57:49  | guifications
-#  12 years 3 mons 8 days 11:42:49   | gliv
-#  12 years 2 mons 19 days 04:25:19  | abootimg
-#  12 years 2 mons 8 days 06:27:16   | ipwatchd
-#  12 years 2 mons 8 days 04:27:24   | ipwatchd-gnotify
-#  12 years 2 mons 4 days 00:56:41   | nullidentd
-#  12 years 1 mon 21 days 04:27:24   | bs2b-ladspa
-#  12 years 1 mon 16 days 04:25:46   | zmakebas
-#  12 years 18 days 22:27:10         | oggfwd
-#  12 years 18 days 08:42:51         | gtkguitune
-#  12 years 15 days 09:11:38         | darnwdl
-#  12 years 10 days 05:12:53         | libspctag
-#  12 years 4 days 17:27:20          | libprintsys
-#  12 years 3 days 08:39:43          | gpr
-#  11 years 11 mons 29 days 07:25:10 | xlassie
-#  11 years 10 mons 19 days 02:57:04 | gromit
-#  11 years 10 mons 16 days 22:16:26 | uapevent
-#  11 years 10 mons 10 days 08:12:20 | s3switch
-#  11 years 10 mons 1 day 01:01:39   | cdcd
-#  11 years 9 mons 14 days 09:02:29  | spacezero
-#  11 years 8 mons 22 days 18:55:16  | xiterm+thai
-#  11 years 8 mons 22 days 16:12:36  | gcx
-#  11 years 8 mons 17 days 08:27:48  | codfis
-#  11 years 8 mons 13 days 22:27:35  | komi
-#  11 years 8 mons 19:57:49          | dv4l
-#  11 years 7 mons 8 days 08:57:37   | enum
-#  11 years 6 mons 25 days 00:56:48  | pam-tmpdir
-#  11 years 6 mons 8 days 14:57:04   | vmfs-tools
-#  11 years 6 mons 2 days 06:12:34   | libencode-eucjpms-perl
-#  11 years 5 mons 10 days 04:56:14  | nuttcp
-#  11 years 5 mons 7 days 12:27:49   | blahtexml
-#  11 years 5 mons 1 day 23:12:49    | aggregate
-#  11 years 5 mons 02:01:42          | liboglappth
-#  11 years 4 mons 24 days 03:05:49  | pcal
-#  11 years 4 mons 22 days 11:10:00  | rtfilter
-#  11 years 4 mons 22 days 04:49:29  | sic
-#  11 years 4 mons 21 days 16:26:28  | kbdd
-#  11 years 4 mons 08:11:40          | cvsd
-#  11 years 3 mons 27 days 00:55:40  | logtop
-#  11 years 3 mons 14 days 17:42:38  | fbautostart
-#  11 years 3 mons 14 days 11:57:48  | dnstop
-#  11 years 3 mons 10 days 03:04:23  | wmctrl
-#  11 years 3 mons 6 days 06:56:49   | gbemol
-#  11 years 3 mons 5 days 01:55:35   | fuse-convmvfs
-#  11 years 3 mons 4 days 07:42:28   | yorick-curses
-#  11 years 3 mons 4 days 07:19:41   | yorick-imutil
-#  11 years 3 mons 4 days 07:08:55   | yorick-soy
-#  11 years 3 mons 4 days 02:37:10   | rsstail
-#  11 years 3 mons 3 days 00:52:19   | pidgin-mra
-#  11 years 3 mons 2 days 14:47:46   | mumudvb
-#  11 years 2 mons 27 days 02:56:20  | ruby-fast-xs
-#  11 years 2 mons 23 days 16:57:40  | vorbisgain
-#  11 years 2 mons 18 days 10:42:51  | pavumeter
-#  11 years 1 mon 27 days 14:12:50   | cccd
-#  11 years 1 mon 27 days 01:12:18   | uniutils
-#  11 years 1 mon 15 days 13:59:53   | libdr-sundown-perl
-#  11 years 1 mon 15 days 04:12:05   | ztex-bmp
-#  11 years 16 days 16:42:16         | espctag
-#  11 years 2 days 12:59:55          | utfout
-#  10 years 11 mons 22 days 08:42:23 | yforth
-#  10 years 10 mons 15 days 12:59:48 | tmfs
-#  10 years 9 mons 14 days 11:57:28  | display-dhammapada
-#  10 years 7 mons 13 days 02:59:51  | fpgatools
-#  10 years 7 mons 11 days 06:57:32  | eot-utils
-#  10 years 7 mons 11 days 06:57:25  | multiwatch
-#  10 years 5 mons 28 days 08:56:17  | pidgin-latex
-#  10 years 5 mons 9 days 10:27:28   | dfu-programmer
-#  10 years 4 mons 27 days 20:53:39  | lua-augeas
-#  10 years 4 mons 23 days 10:56:42  | i810switch
-#  10 years 4 mons 3 days 00:12:03   | apache-upload-progress-module
-#  10 years 4 mons 2 days 11:11:09   | premake4
-#  10 years 4 mons 16:57:07          | cultivation
-#  10 years 3 mons 27 days 12:40:28  | unagi
-#  10 years 3 mons 25 days 02:40:44  | libdumb
-#  10 years 3 mons 23 days 20:10:58  | xjobs
-#  10 years 3 mons 23 days 13:40:59  | mdm
-#  10 years 3 mons 23 days 13:26:47  | digitools
-#  10 years 3 mons 17 days 07:39:57  | libapache2-mod-authn-sasl
-#  10 years 3 mons 16 days 08:41:51  | binutils-z80
-#  10 years 3 mons 13 days 13:26:36  | libapache2-mod-xsendfile
-#  10 years 3 mons 12 days 06:41:55  | tclodbc
-#  10 years 2 mons 29 days 13:41:38  | mancala
-#  10 years 2 mons 29 days 10:56:38  | quadrule
-#  10 years 2 mons 21 days 16:41:00  | mod-vhost-ldap
-#  10 years 2 mons 21 days 02:37:12  | libapache2-mod-ldap-userdir
-# (100 rows)
+
+
+if False:
+    # Bookworm, which has been released.
+    # https://wiki.debian.org/DebianReleases#Production_Releases
+    # --> Bookworm was 2023-06-10
+    # FIXME: only 1 of these ages is negative.
+    #        If this could "see" bookworm security updates, there should be more, surely?
+    #        There is no row for "chromium" at all, for example!
+    #
+    #            bash5$ ssh tweak psql postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd <<< "SELECT s.release, s.source, s.version, u.date from sources s LEFT OUTER JOIN upload_history u USING (source, version) where source = 'chromium'"
+    #                      release          |  source  |         version          |          date
+    #            ---------------------------+----------+--------------------------+------------------------
+    #             stretch-security          | chromium | 72.0.3626.122-1~deb9u1   |
+    #             stretch                   | chromium | 73.0.3683.75-1~deb9u1    |
+    #             stretch-security          | chromium | 73.0.3683.75-1~deb9u1    |
+    #             buster                    | chromium | 89.0.4389.114-1~deb10u1  |
+    #             buster-security           | chromium | 89.0.4389.114-1~deb10u1  |
+    #             buster                    | chromium | 90.0.4430.212-1~deb10u1  |
+    #             buster-security           | chromium | 90.0.4430.212-1~deb10u1  |
+    #             bullseye                  | chromium | 116.0.5845.180-1~deb11u1 |
+    #             bookworm                  | chromium | 119.0.6045.199-1~deb12u1 |
+    #             bullseye-proposed-updates | chromium | 120.0.6099.129-1~deb11u1 |
+    #             bookworm-proposed-updates | chromium | 120.0.6099.199-1~deb12u1 |
+    #             bullseye-security         | chromium | 120.0.6099.216-1~deb11u1 |
+    #             bookworm-security         | chromium | 120.0.6099.216-1~deb12u1 |
+    #             sid                       | chromium | 120.0.6099.216-1         | 2024-01-10 09:14:38+00
+    #             trixie                    | chromium | 120.0.6099.216-1         | 2024-01-10 09:14:38+00
+    #            (15 rows)
+    query = """SELECT age(TIMESTAMPTZ '2023-06-10', max(u.date)) AS age, u.source FROM upload_history u JOIN sources s USING (source, version) WHERE s.release LIKE 'bookworm%' GROUP BY u.source ORDER BY age DESC LIMIT 100000;"""
+elif False:
+    # Trixie, current testing, so use "today" as the date.
+    query = """SELECT age(max(u.date)) AS age, u.source FROM upload_history u JOIN sources s USING (source, version) WHERE s.release LIKE 'trixie%' GROUP BY u.source ORDER BY age DESC LIMIT 100000;"""
+elif False:
+    # Unstable , so use "today" as the date.
+    query = """SELECT age(max(u.date)) AS age, u.source FROM upload_history u JOIN sources s USING (source, version) WHERE s.release = 'sid' GROUP BY u.source ORDER BY age DESC LIMIT 100000;"""
+else:
+    # Of the packages that *exist in Debian Stable*, what is their most recent upload to *any* release?
+    # (This omits packages that are in upload_history but were removed from Debian long ago, e.g. staroffice3-installer)
+    query = """SELECT date_part('year', max(date)) AS "year of latest upload (to sid)", source AS "source package (in bookworm)" FROM upload_history JOIN sources USING (source) WHERE release IN ('bookworm') GROUP BY 2 ORDER BY 1, 2"""
+    # UPDATE: omit packages update in the last 2 years, so the file size is <0.5MiB, so github js CSV rendering kicks in.
+    #         https://docs.github.com/en/repositories/working-with-files/using-files/working-with-non-code-files#rendering-csv-and-tsv-data
+    query = """SELECT date_part('year', max_date) AS "year of latest upload (to sid)", source AS "source package (in bookworm)" FROM (SELECT max(date) AS max_date, source FROM upload_history JOIN sources USING (source) WHERE release IN ('bookworm') GROUP BY source ORDER BY max_date, source) AS fuck_off_pg WHERE max_date < (now() - INTERVAL '2 years')"""
+    query = """SELECT date_part('year', max(date)) AS "year of latest upload (to sid)", source AS "source package (in bookworm)" FROM upload_history JOIN sources USING (source) WHERE release IN ('bookworm') GROUP BY 2 ORDER BY 1, 2 LIMIT 10000"""
+
+
+subprocess.check_call(['psql', 'postgresql://udd-mirror:udd-mirror@udd-mirror.debian.net/udd', '--csv', '-c', query])
