4377d506761b095201b6a392f0d194cf69c30871
Trent W. Buck <trentbuck@gmail.com>
2024-05-27 14:22:00 +1000

    Someone made Wesnoth 1.18 available as a Debian 12 Backport. Therefore update the PrisonPC extra DLCs package to match. https://alloc.cyber.com.au/task/task.php?taskID=35142

--- debian-12-PrisonPC.packages/prisonpc-wesnoth-extras/debian/rules.py
+++ debian-12-PrisonPC.packages/prisonpc-wesnoth-extras/debian/rules.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import configparser
+import logging
 import pathlib
 import subprocess
 import tempfile
@@ -7,11 +8,16 @@ import tempfile
 # FIXME: convert this to toml.
 lookup_table = configparser.ConfigParser()
 lookup_table.read('debian/addons.ini')
-addons = frozenset(
-    set(lookup_table['addons'].get('TODO', '').split()) |
-    set(lookup_table['addons'].get('PASS', '').split()))
+good_set = set(lookup_table['addons'].get('PASS', '').split())
+good_set |= set(lookup_table['addons'].get('TODO', '').split())
+shit_set = set(lookup_table['addons'].get('FAIL', '').split())
+addons = frozenset(good_set - shit_set)
 
-destdir = pathlib.Path('debian/prisonpc-wesnoth-addons/usr/share/games/wesnoth/1.16/data/add-ons/')
+if good_set & shit_set:
+    logging.warning('These DLCs are in both FAIL and PASS/TODO: %s', good_set & shit_set)
+
+
+destdir = pathlib.Path('debian/prisonpc-wesnoth-addons/usr/share/games/wesnoth/1.18/data/add-ons/')
 destdir.mkdir(exist_ok=True, parents=True)
 with tempfile.TemporaryDirectory() as td_str:
     td = pathlib.Path(td_str)
@@ -22,7 +28,7 @@ with tempfile.TemporaryDirectory() as td_str:
             # FIXME: use https (not http) when debugging is done.
             # NOTE: https:// currently broken for gnutls (inc. wget/wget2/curl) due to
             #           issuer DST Root CA X3,O=Digital Signature Trust Co.
-            print(f'http://files.wesnoth.org/addons/1.16/{addon}.tar.bz2', file=f)
+            print(f'http://files.wesnoth.org/addons/1.18/{addon}.tar.bz2', file=f)
     subprocess.run(
         ['wget2',
          '--http-proxy', 'http://localhost:3142/',  # FIXME: remove when done debugging
