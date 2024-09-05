d3e54cca61c9be994409d517d233deb08e34df10
Trent W. Buck <trentbuck@gmail.com>
2024-09-05 13:57:34 +1000

    POC using VM desktop, but real PrisonPC main server
    Running on tweak so that it uses tcb.py and apache and the shitty in-house cert from there, as-is.
    
    Initially it fails because linuxx64.efi served by tcb.py is not signed by the nonce cert.
    
    Then I do this WHILE QEMU IS RUNNING:
    
        cyber@tweak:~$ sudo cp --verbose --backup /tmp/mmdebstrap.*//linuxx64.efi /srv/netboot/images/main-latest/linuxx64.efi
    
    Then in qemu, do ^Ac sendkey ctrl-alt-delete ^J ^Ac
    
    Then it loads the signed linuxx64.efi as shown attached.
    Because there is no SSH port forwarding, and
    there is no console=ttyS0 in the boot args,
    that's as far as I can see in this test.

--- inner.py
+++ inner.py
@@ -10,19 +10,25 @@ subprocess.check_call([
     'ukify', 'genkey',
     '--secureboot-private-key=/tmp/key',
     '--secureboot-certificate=/tmp/cert'])
-subprocess.check_call([
-    'ukify', 'build',
-    '--secureboot-private-key=/tmp/key',
-    '--secureboot-certificate=/tmp/cert',
-    '--linux=/vmlinuz',
-    '--initrd=/initrd.img',
-    '--cmdline=console=ttyS0 earlyprintk=ttyS0 loglevel=2 break'])
+# subprocess.check_call([
+#     'ukify', 'build',
+#     '--secureboot-private-key=/tmp/key',
+#     '--secureboot-certificate=/tmp/cert',
+#     '--linux=/vmlinuz',
+#     '--initrd=/initrd.img',
+#     '--cmdline=console=ttyS0 earlyprintk=ttyS0 loglevel=2 break'])
 subprocess.check_call([
     'sbsign',
     '--key=/tmp/key',
     '--cert=/tmp/cert',
     '--output=/snponly_x64.efi',
     '/snponly_x64.unsigned.efi'])
+subprocess.check_call([
+    'sbsign',
+    '--key=/tmp/key',
+    '--cert=/tmp/cert',
+    '--output=/linuxx64.efi',   # <-------- IMPORTANT NOTE YOU MUST COPY THIS INTO /srv/netboot/images MANUALLY, BECAUSE THIS SCRIPT LACKS PERMISSIONS TO DO SO
+    '/linuxx64.unsigned.efi'])
 uuid_str = uuid.uuid4().hex
 subprocess.check_call([
     'virt-fw-vars',
@@ -32,41 +38,42 @@ subprocess.check_call([
     '--add-kek', uuid_str, '/tmp/cert',  # because PK can't sign EFIs
     '--add-db', uuid_str, '/tmp/cert',  # because KEK is ALSO not working??!!?
     '--output', '/tmp/OVMF_VARS_4M.fd'])
-# Create a fucking ESP because we can't just pass the UKI directly via qemu -kernel?
-subprocess.check_call(['truncate', '/tmp/vda', '--size', '64M'])
-subprocess.check_call([
-    'parted', '--script', '--align=optimal', '/tmp/vda',
-    'mklabel gpt',
-    'mkpart FUCK 1048576b 100%',
-    'set 1 esp on'])
-subprocess.check_call(      # ≈ mkfs.vfat
-    ['mformat', '-i', '/tmp/vda@@1048576',
-     '-F', '-v', 'FUCK'])
-with tempfile.TemporaryDirectory() as td_str:
-    td = pathlib.Path(td_str)
-    (td / 'EFI').mkdir()
-    (td / 'EFI/BOOT').mkdir()
-    # (td / 'EFI/BOOT/BOOTX64.EFI').write_bytes(pathlib.Path('/vmlinuz.efi').read_bytes())
-    # (td / 'EFI/BOOT/BOOTX64.EFI').write_bytes(pathlib.Path('/snponly_x64.efi').read_bytes())
-    subprocess.check_call(      # ≈ mount, cp, umount
-        ['mcopy', '-i', '/tmp/vda@@1048576',
-         '-vspm',
-         'EFI',                     # source dirs
-         '::'],                     # destdir is root of fs
-        cwd=td)
+# # Create a fucking ESP because we can't just pass the UKI directly via qemu -kernel?
+# subprocess.check_call(['truncate', '/tmp/vda', '--size', '64M'])
+# subprocess.check_call([
+#     'parted', '--script', '--align=optimal', '/tmp/vda',
+#     'mklabel gpt',
+#     'mkpart FUCK 1048576b 100%',
+#     'set 1 esp on'])
+# subprocess.check_call(      # ≈ mkfs.vfat
+#     ['mformat', '-i', '/tmp/vda@@1048576',
+#      '-F', '-v', 'FUCK'])
+# with tempfile.TemporaryDirectory() as td_str:
+#     td = pathlib.Path(td_str)
+#     (td / 'EFI').mkdir()
+#     (td / 'EFI/BOOT').mkdir()
+#     # (td / 'EFI/BOOT/BOOTX64.EFI').write_bytes(pathlib.Path('/vmlinuz.efi').read_bytes())
+#     # (td / 'EFI/BOOT/BOOTX64.EFI').write_bytes(pathlib.Path('/snponly_x64.efi').read_bytes())
+#     subprocess.check_call(      # ≈ mount, cp, umount
+#         ['mcopy', '-i', '/tmp/vda@@1048576',
+#          '-vspm',
+#          'EFI',                     # source dirs
+#          '::'],                     # destdir is root of fs
+#         cwd=td)
 # Actually boot it.
-pathlib.Path('/netboot.ipxe').write_text('#!ipxe\nboot --replace vmlinuz.efi\n')
-pathlib.Path('/netboot.ipxe').write_text('#!ipxe\nboot --replace snponly_x64.efi\n')
+# pathlib.Path('/FUCK.IPXE').write_text('#!ipxe\nboot --replace vmlinuz.efi\n')
+pathlib.Path('/FUCK.IPXE').write_text('#!ipxe\nboot --replace snponly_x64.efi\n')
 subprocess.check_call([
     'qemu-system-x86_64',
     '-nographic',  # WARNING: still needs a 2D terminal, because OVMF config screens requires it :/
     '-nic', 'none',  # make OVMF not try to fall back to onboard iPXE, for now
     '-device', 'virtio-net-pci,netdev=OutclassMountingBoggle',
-    '-netdev', 'id=OutclassMountingBoggle,type=user,tftp=/,bootfile=netboot.ipxe',
+    '-netdev', 'id=OutclassMountingBoggle,type=user,tftp=/,bootfile=FUCK.IPXE,dnssearch=lan',
     '-machine', 'q35,smm=on',
     '-m', '1G',   # SIGH: "BdsDxe: failed to load ⋯: Out of Resources"
     '-global', 'driver=cfi.pflash01,property=secure,value=on',
     '-drive', 'if=pflash,format=raw,unit=0,file=/usr/share/OVMF/OVMF_CODE_4M.secboot.fd,readonly=on',
     '-drive', 'if=pflash,format=raw,unit=1,file=/tmp/OVMF_VARS_4M.fd,readonly=off',
-    '--drive', 'if=none,id=FUCK,file=/tmp/vda,format=raw,readonly=on',
-    '--device', 'virtio-blk-pci,drive=FUCK,serial=DUCK'])
+    # '--drive', 'if=none,id=FUCK,file=/tmp/vda,format=raw,readonly=on',
+    # '--device', 'virtio-blk-pci,drive=FUCK,serial=DUCK'
+])
--- outer.py
+++ outer.py
@@ -3,9 +3,13 @@
 import subprocess
 subprocess.check_call([
     'mmdebstrap', 'trixie', '/dev/null',
-    '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
+    '--aptopt=Acquire::http::Proxy "http://apt-cacher-ng.cyber.com.au:3142"',
     '--aptopt=Acquire::https::Proxy "DIRECT"',
     '--include=python3 linux-image-cloud-amd64 systemd-ukify systemd-boot sbsigntool qemu-system-x86 ovmf python3-virt-firmware uuid-runtime parted mtools',
-    '--customize-hook=upload snponly_x64.efi /snponly_x64.unsigned.efi',
+    # FAILS BECAUSE -r-------- 1 dnsmasq nogroup 228352 Jan  1  2024 /usr/share/prisonpc/ipxe/snponly_x64.efi
+    # '--customize-hook=upload /usr/share/prisonpc/ipxe/snponly_x64.efi /snponly_x64.unsigned.efi',
+    # use a manually-created local copy
+    '--customize-hook=upload ./snponly_x64.efi /snponly_x64.unsigned.efi',
+    '--customize-hook=upload /srv/netboot/images/main-latest/linuxx64.efi /linuxx64.unsigned.efi',
     '--customize-hook=copy-in inner.py /',
     '--customize-hook=chroot $1 python3 inner.py'])
