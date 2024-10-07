8d4154b8ba027d3ad1f0562d084e785b578c841c
Trent W. Buck <trentbuck@gmail.com>
2024-10-08 10:12:21 +1100

    Revert "libnss-ldapd is multiarch now, I guess?"
    This reverts commit 4535ccb1ce77edb7ddd29389dc1414e4b14dff57.
    
    I definitely misunderstood why I was seeing the prompt in interactive usage in prisonpc.git.

--- debian-12-PrisonPC.hooks/essential50-enable-libnss-ldapd.py
+++ debian-12-PrisonPC.hooks/essential50-enable-libnss-ldapd.py
@@ -10,6 +10,6 @@ parser.add_argument('chroot_path', type=pathlib.Path)
 args = parser.parse_args()
 subprocess.run(
     ['chroot', args.chroot_path, 'debconf-set-selections'],
-    input='libnss-ldapd:amd64 libnss-ldapd/nsswitch multiselect passwd group',
+    input='libnss-ldapd libnss-ldapd/nsswitch multiselect passwd group',
     check=True,
     text=True)
