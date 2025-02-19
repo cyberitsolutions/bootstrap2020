e60f6ecdb2ff2e436364ffbb59b7d7c323371b71
Trent W. Buck <trentbuck@gmail.com>
2025-02-19 19:41:21 +1100

    use Debian 12 tvserver in production

--- debian-12-production.py
+++ debian-12-production.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# FIXME: merge this "preset" and "loop" functionality into main.py
-
-import datetime              # FIXME: workaround for broken TBS driver
 import subprocess
 
 for args in [
@@ -10,7 +7,7 @@ for args in [
         ['--physical-only',
          '--templates',
          'understudy',
-         # 'tvserver',       # FIXME: workaround for broken TBS driver
+         'tvserver',
          'desktop-inmate-amc',
          'desktop-inmate-amc-library'],
         ['--templates',
@@ -25,17 +22,3 @@ for args in [
         '--upload-to', 'root@tweak.prisonpc.com', 'root@amc.prisonpc.com',
         *args,
     ])
-
-
-# FIXME: workaround for broken TBS driver
-subprocess.check_call([
-    './debian-11-main.py',
-    '--remove',
-    # Hard-code $LANG and $TZ instead of inheriting from build host.
-    '--LANG', 'en_AU.UTF-8', '--TZ', 'Australia/Canberra',
-    '--netboot-only',       # no ISO/USB
-    '--physical-only',
-    '--ssh=openssh-server',  # PrisonPC needs this
-    f'--reproducible={datetime.date.today()}',
-    '--upload-to', 'root@tweak.prisonpc.com', 'root@amc.prisonpc.com',
-    '--template', 'tvserver'])
