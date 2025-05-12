94b94343264f02a7bb8910512fd684bafeb6792b
Trent W. Buck <trentbuck@gmail.com>
2025-05-12 20:00:07 +1000

    Adblock via /etc/hosts any better?

--- debian-12-PrisonPC.hooks/customize50-chromium-adblockminus.py
+++ debian-12-PrisonPC.hooks/customize50-chromium-adblockminus.py
@@ -2,14 +2,15 @@
 import argparse
 import json
 import pathlib
+import re
 import requests
 
-import jsmin
 
 __doc__ = """ bare-bones 60% ad/telemetry blocker
+
 Detainees have a default-deny list, where EVERYTHING is blocked unless it is explicitly allowed.
 Staff have a default-allow list, but still have plugins disabled, so they can't install ad blockers even if they want to.
-We also can't easily ship actual an adblock-plus plugin without other security tradeoffs.
+We also can't easily ship actual an ublock-origin (née adblock-plus) plugin without other security tradeoffs.
 
 We mostly consider this FineTM - if staff want to do personal browsing
 from their PrisonPC staff account instead of their government account,
@@ -23,7 +24,7 @@ Just to make *that analysis* easier next time, do some absolute bare-bones ad bl
 
 This adapts some ancient code I (twb) had set up personally to block things via /etc/hosts, so
 that the blocking (i.e. protection) worked even if you were not using a GUI browser.
-It looks at the some third-party block lists for AdBlock Plus,
+It looks at the some third-party block lists for uBlock Origin,
 looks for anything that matches an exact domain, and adds that to Chromium's URLBlocklist.
 
 This doesn't deal with all the e.g. "/trackingpixel.gif" rules.
@@ -35,15 +36,32 @@ NOTE: this downloads the block lists are SOE build time, so newer SOE = newer bl
 NOTE: if this causes problems, you can disable it by creating an empty
       file of the same name in site.dir/etc/chromium/policies/managed/.
 
-FIXME: integrate this list?  http://adaway.org/hosts.txt
-FIXME: The set of list we're using hasn't been reviewed in forever (i.e. are there better lists?)
+UPDATE: URLAllowlist & URLDenylist each have a limit of 1000 records, and
+        EasyList/EasyPrivacy has ~70,000 rules even just for host-wide rules.
+        Therefore doing a full block list there is definitely untenable.
+        We *can* add the records to /etc/hosts,
+        although systemd-resolved requires a fair amount of RAM for those, AND
+        they are not recursive (e.g. adding "example.com" won't stop "www.example.com".
+
+        Let's try it anyway and see how bad it is...
+
+        UPDATE: can't actually write to /etc/hosts directly, because
+        some useless pre-systemd code in live-boot is replacing
+        /etc/hosts outright at boot time.
+
+        UPDATE: resolved w/ empty hosts = ~20MB
+                resolved w/ 70K rules in hosts = ~40MB --- meh
+
+        UPDATE: also this edit to /etc/hosts doesn't change v6 resolution.
 
 See also:
 
+   https://en.wikipedia.org/wiki/UBlock_Origin#uBlock_Origin_Lite
    https://chromeenterprise.google/policies/#URLBlocklist
    https://adblockplus.org/en/subscriptions
    https://adblockplus.org/filter-cheatsheet
    https://help.adblockplus.org/hc/en-us/articles/360062733293-How-to-write-filters
+   moz-extension://2ba1e40e-fb8e-4267-b7f7-b1fb316f0528/dashboard.html#3p-filters.html
 
 """
 
@@ -51,24 +69,38 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('chroot_path', type=pathlib.Path)
 args = parser.parse_args()
 
-dest_dir = args.chroot_path / 'etc/chromium/policies/managed'
+hosts_path =  args.chroot_path / 'etc/hosts'
 list_urls = [
-    'https://easylist.to/easylist/easyprivacy.txt',
-    'https://easylist.to/easylist/easylist.txt',
-    'https://easylist.to/easylist/fanboy-annoyance.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/filters.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/badware.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/privacy.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/quick-fixes.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/unbreak.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/thirdparties/easylist/easylist.txt',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/thirdparties/easylist/easyprivacy.txt',
+    # Different syntax -- too hard for now
+    # 'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/thirdparties/urlhaus-filter/urlhaus-filter-online.txt',
+    # Different syntax -- too hard for now
+    # 'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/thirdparties/pgl.yoyo.org/as/serverlist',
+    'https://github.com/uBlockOrigin/uAssets/raw/refs/heads/master/filters/resource-abuse.txt',
 ]
-for list_url in list_urls:
-    resp = requests.get(list_url)
+
+def requests_get(url: str) -> str:
+    """requests.get(), but raise exceptions on 4xx/5xx"""
+    resp = requests.get(url)
     resp.raise_for_status()
-    # Consider *only* complete domain blocks ("||example.com^").
-    # Everything else is in the "too hard" basket for now.
-    domains = [
-        rule[len('||'):-len('^')]
-        for rule in resp.text.splitlines()
-        if rule.startswith('||') and rule.endswith('^')
-        if '/' not in rule
-    ]
-    dest_path = dest_dir / f'10-URLs.json'
-    policy_dict = json.loads(jsmin.jsmin(dest_path.read_text()))
-    policy_dict['URLBlocklist'] = policy_dict.get('URLBlocklist', {}) + domains
-    dest_path.write_text(json.dumps(policy_dict))
+    return resp
+
+domains = sorted(
+    {rule
+     for list_url in list_urls
+     for rule in re.findall(
+             r'(?m)^\|\|([a-z0-9.-]+)\^$',  # just FQDNs (FIXME: still has IPs)
+             requests_get(list_url).text)},
+    key=(lambda url: list(reversed(url.split('.')))))
+
+with hosts_path.open('a') as f:
+    for domain in domains:
+        # In principle we could put all these on a single line.
+        # In practice firefox-esr, at least, seems to crash when a single /etc/hosts line is too long.
+        print(f'127.255.255.254 {domain:>64}', file=f)
