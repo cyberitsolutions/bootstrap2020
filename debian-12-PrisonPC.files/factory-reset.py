27100d070f93a023f861e498f7ea18899a40a5dc
Trent W. Buck <trentbuck@gmail.com>
2025-07-09 18:13:44 +1000

    factory-reset: overhaul
     * mv-then-rm (not in-place shutils.rmtree)
     * therefore, don't need to try to pre-kill chromium, libreoffice, pulseaudio/pipewire
     * incidentally avoids warnings from systemctl in removed code
    
     * logout prompt (don't try to force logout)
     * incidentally avoids errors from pkill in removed code
    
     * reword the initial user-facing prompt to manage expectations a bit better

--- debian-12-PrisonPC.files/factory-reset.py
+++ debian-12-PrisonPC.files/factory-reset.py
@@ -6,15 +6,12 @@
 #
 # NOTE: we use os.listdir instead of glob() because ~ may contain glob
 # special characters.  Ref. https://www.python.org/sf/8402
+# UPDATE: glob.glob() was bad, but Path.glob() is fine. --twb, July 2025
 
 
-import errno
 import logging
-import os
 import pathlib
-import shutil
 import subprocess
-import sys
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -51,11 +48,17 @@ import gi.repository.Gtk        # noqa: E402
 #
 # Ref. http://sources.debian.net/src/xdm/1:1.1.8-5/auth.c/#L1256
 # Ref. http://sources.debian.net/src/xdm/1:1.1.11-3/xdm/auth.c/?hl=1500#L1344
-MAGIC_XDM_DIR = '.Xauthority-c'
+# Ref. https://manpages.debian.org/bookworm/libxau-dev/XauLockAuth.3.en.html#DESCRIPTION
 
 # See also https://sources.debian.org/src/zenity/3.32.0-6/src/zenity.ui/#L879-L949
 # We do not use zenity because it pulls in webkitgtk, which
 # has no security support.
+#
+# NOTE: "quit all apps" is not technically required, but
+#       minimizes the chance of weirdness if apps are left open (or started)
+#       in between factory-reset stating, and the post-reset logout.
+#       We round this down to "quit all apps" to simplify the message to detainees.
+#       --twb, July 2025.
 dialog = gi.repository.Gtk.MessageDialog(
     title='Factory Reset',
     message_type=gi.repository.Gtk.MessageType.WARNING,
@@ -63,37 +66,13 @@ dialog = gi.repository.Gtk.MessageDialog(
 dialog.set_markup(
     'All settings will be reset to PrisonPC defaults.\n'
     'Regular office documents will <i>not</i> be affected.\n'
-    'You will be logged out.\n'
+    'This may take up to 1 minute without feedback.\n'
+    'When finished a logout prompt will appear.\n'
+    'Log out to complete the factory reset.\n'
+    '\n'
+    'Save your work and quit all apps before continuing.\n'
     '\n'
     '<b>Reset account to factory defaults?</b>\n')
-
-
-# NFSv3, like Windows, won't let you unlink open files.
-# Instead it renames the inode to a temporary name like ./.nfsN.
-# When the last file handle is closed, it's deleted properly.
-#
-# This means we can't simple use "rm -rf",
-# because it'll abort on the first open file handle.
-# We can't kill all those processes, because we need some.
-#
-# NOTE: I don't understand why, but omitting the "return" lines,
-# or changing them to "pass", breaks the handler.
-# Likewise "return False" instead of "raise". --twb, Sep 2016
-def handle_error(function, path, excinfo):
-    _, exception, _ = excinfo
-    if ((function in (os.remove, os.unlink)
-         and isinstance(exception, OSError)  # noqa: W503
-         and exception.errno == errno.EBUSY  # noqa: W503
-         and pathlib.Path(path).name.startswith('.nfs'))):  # noqa: W503
-        logging.warning('Ignoring probable stale NFS inode: %s', exception)
-        return
-    if ((function == os.rmdir
-         and isinstance(exception, OSError)        # noqa: W503
-         and exception.errno == errno.ENOTEMPTY)):  # noqa: W503
-        logging.warning('Ignoring probable parent dir stale NFS inode: %s', exception)
-        return
-    else:
-        raise
 
 
 if gi.repository.Gtk.ResponseType.YES == dialog.run():
@@ -106,56 +85,51 @@ if gi.repository.Gtk.ResponseType.YES == dialog.run():
     logging.getLogger().setLevel(logging.INFO)
     logging.info('user-initiated factory reset starting')
 
-    # Explicitly terminate some GUI apps that are particularly problematic.
-    # Ignore errors because if they aren't running or don't terminate, we mostly don't care.
-    subprocess.call(['pkill', '-9', 'chromium|soffice.bin'])
-    subprocess.call(['systemctl', '--user', 'stop',
-                     # These were problems in Debian 11, but aren't in Debian 12.
-                     'pulseaudio.service', 'pulseaudio.socket',
-                     # These are in Debian 12 -- are they are problem?
-                     # FIXME: find out, instead of assuming they are a problem.
-                     'pipewire-pulse.service',
-                     'pipewire-pulse.socket',
-                     'pipewire.service',
-                     'pipewire.socket',
-                     'wireplumber.service',
-                     ])
-
+    # NOTE: "mv" is MUCH faster than "rm -rf".
+    #       So we first move everything to a staging area, then delete that.
+    #       Hopefully if a detainee tries to run an app mid-delete,
+    #       this means the app will see an immediately-clean area,
+    #       instead of a half-deleted area.
+    #       Hopefully this means we can skip the "try to kill bad apps"
+    #       code which was buggy af. --twb, July 2025
+    #
+    # NOTE: NFSv4, like Windows, won't let you unlink open files.
+    #       Instead it renames the inode to a temporary name like ./.nfsXXXXXX.
+    #       When the last file handle is closed, it's auto-deleted properly by the server.
+    #       Until then you cannot rename the file (to another dir) or delete it.
+    #       You can move its parent directory, though.
+    #
+    #       Python shutils.rmtree (and pathlib.Path.rename?) will crash on the first .nfsXXXXXX file.
+    #       GNU mv and GNU rm -rf will complain to stderr, but continue doing what they can.
+    #       That's why this script is using subprocess.run() instead of native Python functions.
+    #
+    # NOTE: I wanted to use trash-put to move everything into trash://,
+    #       then (optionally) empty trash.
+    #       This was more complicated (e.g. ~/.local/share/Trash/ vs ~/.Trash-<UID>/).
+    #       It ended up being Too Hard for now. --twb, July 2025
     home = pathlib.Path.home()
     if not home.is_relative_to('/home'):
         raise RuntimeError('suspicious $HOME', home)
-    for path in home.glob('.*'):  # Only remove hidden files!
-        # FIXME: is this "magic" still useful in Debian 11???
-        if path.name == MAGIC_XDM_DIR:
-            logging.debug('Skipping magic XDM dir')
-            continue
-        # NOTE: os.walk was MUCH SLOWER than shutil.rmtree.
-        if path.is_dir() and not path.is_symlink():
-            shutil.rmtree(path, onerror=handle_error)
-        else:
-            # rmtree() refuses to remove symlinks and regular files,
-            # so we have to repeat ourselves here.  SIGH.
-            # Minimize duplication by re-using the handler.
-            try:
-                path.unlink()
-            except OSError:
-                handle_error(os.remove, path, sys.exc_info())
+    staging_area_path = home / '.PrisonPC-factory-reset'
+    magic_xdm_path = home / '.Xauthority-c'
+    subprocess.run(['rm', '-rf', '--', staging_area_path], check=True)
+    staging_area_path.mkdir()
+    subprocess.run(
+        ['mv', '-t', staging_area_path, '--',
+         *(path
+           for path in home.glob('.*')  # Only remove hidden files!
+           if path != staging_area_path
+           if path != magic_xdm_path)],
+        check=True)
+    # This will usually fail to remove some open files & their parent dirs.
+    # We do not care, because the space consumed is negligible.
+    subprocess.run(['rm', '-rf', '--', staging_area_path], check=False)
 
     # Just in case the user manually deleted it...
-    subprocess.check_call(['install', '-d', home / MAGIC_XDM_DIR])
+    magic_xdm_path.mkdir(parents=False, exist_ok=True)
 
     logging.info('user-initiated factory reset completed without error')
 
-    # Force a logout in the simplest way:
-    # force-kill any process we're allowed to.
-    # Killing xfce4-session only might be cleaner,
-    # but might silently fail when the session start processes change.
-    # --twb, Sep 2016
-    #
-    # UPDATE: AMC was reporting that "the desktop didn't close & reboot was not forced".
-    #         I speculate that "pkill -9 ." was killing ITSELF BEFORE the XFCE stuff.
-    #         To avoid this, first try killing the desktop.
-    #         Then try to kill everything if that failed.
-    subprocess.call(['pkill', '-f', 'Xsession|x-session-manager'])
-    subprocess.call(['systemctl', '--user', 'stop', '*'])
-    subprocess.check_call(['pkill', '-9', '.'])
+    # Force a logout in the simplest way: [...]
+    # UPDATE: this was buggy af, so instead just act like the user clicked Applications > Log Out.
+    subprocess.call(['xfce4-session-logout'])
