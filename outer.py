aa4a7297cb62bf1f5f57939ede4d37fa3d1b1e68
Trent W. Buck <trentbuck@gmail.com>
2025-10-08 13:46:53 +1100

    fixup! Make D13 minimal REQUIRE Debian 13 build host

--- outer.py
+++ outer.py
@@ -5,7 +5,7 @@ subprocess.check_call([
     'mmdebstrap', 'trixie', '/dev/null',
     '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
     '--aptopt=Acquire::https::Proxy "DIRECT"',
-    '--include=python3 linux-image-cloud-amd64 systemd-ukify systemd-boot sbsigntool qemu-system-x86 ovmf python3-virt-firmware uuid-runtime parted mtools',
+    '--include=python3 linux-image-cloud-amd64 systemd-ukify systemd-boot-efi sbsigntool qemu-system-x86 ovmf python3-virt-firmware uuid-runtime parted mtools',
     # '--customize-hook=upload snponly_x64.efi /snponly_x64.unsigned.efi',
     '--customize-hook=copy-in inner.py /',
     '--customize-hook=chroot $1 python3 inner.py'])
