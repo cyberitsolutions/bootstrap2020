3d9ad5291023e268ac46a53fa8e85eacb99bab8d
Trent W. Buck <trentbuck@gmail.com>
2025-10-17 01:52:46 +1100

    rsbsign: first draft
    This is also the first file in my idea of moving all the .py and
    auxiliary files into a "python module" stack, so that
    
        ruff .
        mypy .
    
    will Just Work.
    Currently mypy gets cranky about having >1 file with the same name.
    
    I expect to do the D13 work in there, and
    probably not bother porting the D11/D12 stuff over (at least initially).
    
    I expect to use importlib.resources for .files and .hooks,
    turning the former directly into a tarball (as now), and
    turning the latter into a tempdir to feed to mmdebstrap --hooks-dir.

---
+++ B20/D13/rsbsign/__main__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+__doc__ = "download, sign, and reupload a remote UKI"
+import argparse
+import pathlib
+import subprocess
+import tempfile
+import time
+
+import pypass                   # https://github.com/aviau/python-pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'host_and_path',
+        help='e.g. root@tweak.prisonpc.com:/srv/netboot/images/desktop-inmate-latest/linuxx64.efi')
+    args = parser.parse_args()
+    if not any(safe_word in args.host_and_path.partition(':')[-1]
+               for safe_word in {'inmate', 'detainee', 'snponly'}):
+        raise RuntimeError('You dickhead, do not sign general-purpose UKIs!', args.host_and_path)
+    now = int(time.time())
+    password_store_path = pathlib.Path('~/.cyber-password-store').expanduser().resolve()
+    password_store = pypass.PasswordStore(path=password_store_path)
+    with tempfile.TemporaryDirectory() as td_str:
+        td = pathlib.Path(td_str)
+        (td / 'key.pem').write_text(
+            password_store.get_decrypted_password('PrisonPC/secure-boot.key.pem'))
+        (td / 'cert.pem').write_text(
+            password_store.get_decrypted_password('PrisonPC/secure-boot.cert.pem'))
+        subprocess.check_call(
+            ['rsync', args.host_and_path, 'unsigned.efi'], cwd=td)
+        if subprocess.run(
+                ['sbverify', '--cert=cert.pem', 'unsigned.efi'],
+                cwd=td).returncode == 0:
+            raise RuntimeError('Already signed by us!')
+        subprocess.check_call(
+            ['sbsign',
+             '--verbose',
+             '--key=key.pem',
+             '--cert=cert.pem',
+             '--output=signed.efi',
+             'unsigned.efi'], cwd=td)
+        # NOTE: sbverify --list does NOT verify it only lists.
+        #       This will normally print just the signature we added.
+        #       If it is already signed by other key(s), it'll also print those.
+        subprocess.check_call(['sbverify', '--list', 'unsigned.efi'], cwd=td)
+        # FIXME: Before upoading, we should update a CSV table somewhere predictable, with
+        #            what, who, when, unsigned cksum, signed cksum
+        #        This way if we need to denylist old UKIs, we at least have a record of them all!
+        #        It has to be the kind of checksum that SB DBx keyring expects!
+        #        (i.e. not BLAKE2s, not RC4 + length).
+        subprocess.check_call(
+            ['rsync', '--backup', f'--suffix=.~{now}~',
+             '--times',
+             'signed.efi', args.host_and_path],
+            cwd=td)
+
+
+if __name__ == '__main__':
+    main()
