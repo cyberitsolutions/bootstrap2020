5e872766ac8fe8e9397ceb620d7e58cd2f2cdea4
Trent W. Buck <trentbuck@gmail.com>
2025-10-17 16:22:31 +1100

    fixup! APT_CONFIG=$MMDEBSTRAP_APT_CONFIG apt (not chroot $1 apt)
    This file was dropped before the APT_CONFIG= change, then
    the drop was reverted.

--- debian-12-desktop.hooks/essential10-fuck.py
+++ debian-12-desktop.hooks/essential10-fuck.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import argparse
+import os
 import pathlib
 import subprocess
 
@@ -9,6 +10,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('chroot_path', type=pathlib.Path)
 args = parser.parse_args()
 
+# 17:05 <twb> Interesting
+# 17:05 <twb> --essential-hook='APT_CONFIG=$MMDEBSTRAP_APT_CONFIG DPKG_ROOT=$1 apt install fontconfig-config' --> confusing error
+# 17:05 <twb> --essential-hook='APT_CONFIG=$MMDEBSTRAP_APT_CONFIG apt install fontconfig-config' --> totally fine
+# 17:06 <twb> In more complicated scripts, I was setting BOTH variables script-wide, because I sometimes run dpkg and sometimes run apt
+# 17:08 <twb> Can't understand why none of the other scripts run into a problem
+os.environ['APT_CONFIG'] = os.environ['MMDEBSTRAP_APT_CONFIG']  # replaces "chroot $1" for apt
+#BROKEN???#os.environ['DPKG_ROOT'] = str(args.chroot_path)  # replaces "chroot $1" for dpkg
+
 subprocess.check_call(
-    ['chronic', 'chroot', args.chroot_path,
-     'apt', 'install', 'fontconfig-config', '--assume-yes'])
+    ['chronic', 'apt', 'install', 'fontconfig-config', '--assume-yes'])
