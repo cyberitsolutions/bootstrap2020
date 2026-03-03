0aed5217e2ee30dbb13bb0e145729554b992ef13
Trent W. Buck <trentbuck@gmail.com>
2026-03-03 22:12:56 +1100

    minimal: D14: refactor a bit

--- B20/minimal/D14.py
+++ B20/minimal/D14.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import argparse
-import pathlib
 import subprocess
-import tempfile
-import textwrap
 
 __author__ = "Trent W. Buck"
 __copyright__ = "Copyright © 2020 Trent W. Buck"
@@ -128,70 +125,48 @@ args = parser.parse_args()
 #                        du -h /tmp/live.img;
 #                    done
 
-with tempfile.TemporaryDirectory(prefix='debian-live-minimal.') as td_str:
-    td = pathlib.Path(td_str)
-    (td / 'repart.d').mkdir()
-    (td / 'repart.d/esp.conf').write_text(textwrap.dedent(
-        """
-        [Partition]
-        Type=esp
-        CopyFiles=/boot:/
-        # SizeMinBytes=16M
-        # SizeMaxBytes=64M
-        # Minimize=guess
-        """))
-    (td / 'repart.d/root.conf').write_text(textwrap.dedent(
-        """
-        [Partition]
-        Type=root
-        Format=squashfs
-        Compression=zstd
-        CompressionLevel=3
-        CopyFiles=/
-        ReadOnly=yes
-        Minimize=yes
-        """))
-    # Make the rootfs
-    subprocess.check_call(
-        ['mmdebstrap', 'forky',
-         # == GO-FASTER STRIPES ==
-         '--mode=unshare',
-         '--variant=apt',
-         '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
-         '--aptopt=Acquire::https::Proxy "DIRECT"',
-         '--dpkgopt=force-unsafe-io',
-         # == KERNEL, RAMDISK, GUEST USER ==
-         '--include=linux-image-generic dracut',
-         '--include=dbus-broker',  # https://bugs.debian.org/814758
-         '--include=login',        # https://bugs.debian.org/960638
-         '--include=live-config iproute2 keyboard-configuration locales sudo user-setup',
-         # == NETWORKING ==
-         # In live-boot+live-config, "do DHCP on any ethernet" is actually in live-boot.
-         # In dracut+live-config, neither is enabled by default.
-         # https://github.com/dracut-ng/dracut-ng/blob/main/modules.d/11systemd-networkd/dracut-default.network
-         '--include=dracut-network systemd-resolved systemd-timesyncd',
-         '--essential-hook=mkdir -p $1/etc/dracut.conf.d/',
-         '''--essential-hook=echo 'add_dracutmodules+=" systemd-network-management "' >$1/etc/dracut.conf.d/50-fuck2.conf''',
-         '--customize-hook=chroot $1 systemctl enable systemd-networkd',
-         # == BOOTLOADER STUB ==
-         # NOTE: boot=live is for live-config (not dracut) <https://bugs.debian.org/1128194>
-         '--include=systemd-ukify systemd-boot-efi',
-         '--customize-hook=mkdir -p $1/boot/efi/boot',
-         '--customize-hook=chroot $1 ukify build --output=boot/efi/boot/bootx64.efi --linux=vmlinuz --initrd=initrd.img --cmdline="systemd.volatile=overlay boot=live"',
-         # == DISK IMAGE ==
-         # NOTE: this uses in-container systemd-repart and mksquashfs,
-         #       rather than mmdebstrap's (better!) tar and tar2sqfs.
-         '/dev/null',
-         '--include=systemd-repart dosfstools mtools squashfs-tools moreutils',
-         '--customize-hook=copy-in repart.d /tmp/',
-         '--customize-hook=chroot $1 chronic systemd-repart --definitions=/tmp/repart.d --offline=yes --empty=create --size=auto /tmp/live.img',
-         f'--customize-hook=copy-out /tmp/live.img {pathlib.Path.cwd().resolve()}',
-         ],
-        cwd=td)
+# Make the rootfs
+subprocess.check_call([
+    'mmdebstrap', 'forky',
+    # == GO-FASTER STRIPES ==
+    '--mode=unshare',
+    '--variant=apt',
+    '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
+    '--aptopt=Acquire::https::Proxy "DIRECT"',
+    '--dpkgopt=force-unsafe-io',
+    # == KERNEL, RAMDISK, GUEST USER ==
+    '--include=linux-image-generic dracut',
+    '--include=dbus-broker',  # https://bugs.debian.org/814758
+    '--include=login',        # https://bugs.debian.org/960638
+    '--include=live-config iproute2 keyboard-configuration locales sudo user-setup',
+    # == NETWORKING ==
+    # In live-boot+live-config, "do DHCP on any ethernet" is actually in live-boot.
+    # In dracut+live-config, neither is enabled by default.
+    # https://github.com/dracut-ng/dracut-ng/blob/main/modules.d/11systemd-networkd/dracut-default.network
+    '--include=dracut-network systemd-resolved systemd-timesyncd',
+    '--essential-hook=mkdir -p $1/etc/dracut.conf.d/',
+    '''--essential-hook=echo 'add_dracutmodules+=" systemd-network-management "' >$1/etc/dracut.conf.d/50-fuck2.conf''',
+    '--customize-hook=chroot $1 systemctl enable systemd-networkd',
+    # == BOOTLOADER STUB ==
+    # NOTE: boot=live is for live-config (not dracut) <https://bugs.debian.org/1128194>
+    '--include=systemd-ukify systemd-boot-efi',
+    '--customize-hook=mkdir -p $1/boot/efi/boot',
+    "--customize-hook=printf '[UKI]\nLinux=/vmlinuz\nInitrd=/initrd.img\nCmdline=systemd.volatile=overlay boot=live\n' >$1/etc/systemd/ukify.conf",
+    '--customize-hook=chroot $1 ukify build --output=boot/efi/boot/bootx64.efi',
+    # == DISK IMAGE ==
+    # NOTE: this uses in-container systemd-repart and mksquashfs,
+    #       rather than mmdebstrap's (better!) tar and tar2sqfs.
+    '/dev/null',
+    '--include=systemd-repart dosfstools mtools squashfs-tools moreutils',
+    "--customize-hook=mkdir $1/etc/repart.d",
+    "--customize-hook=printf '[Partition]\nType=esp\nCopyFiles=/boot:/\n' >$1/etc/repart.d/50-esp.conf",
+    "--customize-hook=printf '[Partition]\nType=root\nCopyFiles=/\nFormat=squashfs\nCompression=zstd\nCompressionLevel=3\nMinimize=yes\n' >$1/etc/repart.d/50-root.conf",
+    '--customize-hook=chroot $1 chronic systemd-repart --offline=yes --empty=create --size=auto /tmp/live.img',
+    '--customize-hook=copy-out /tmp/live.img .'])
 
 # Workaround https://bugs.debian.org/1129567
 subprocess.check_call(['fallocate', '--dig-holes', 'live.img'])
 
 # NOTE: this invocation is concise, NOT efficient!
 if args.boot_test:
-    subprocess.check_call(['kvm', '-m', '1G', '-bios', 'OVMF.fd', '-hda', 'live.img'])
+    subprocess.check_call(['kvm', '-m', '1G', '-bios', 'OVMF.fd', '-drive', 'file=live.img,format=raw'])
