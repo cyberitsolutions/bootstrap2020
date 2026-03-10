4d60bd7ff33d5c304e4c16cbb71c7a86610fb927
Trent W. Buck <trentbuck@gmail.com>
2026-03-10 17:23:01 +1100

    biggest-desktop: appease mypy; minor refactor; re-measure

--- doc/biggest-desktop.py
+++ doc/biggest-desktop.py
@@ -7,6 +7,8 @@ import tempfile
 # FIXME: with stretch/buster/bullseye uncommented,
 #        task-gnome-desktop doesn't show up.
 #        One of those is breaking I guess?
+#        UPDATE: actually I think it's an off-by-one error somewhere.
+#        It's the last task in tasks that's lost every time...
 distros = [
     'stretch',
     'buster',
@@ -15,14 +17,18 @@ distros = [
     'trixie',
     'forky',
     'sid']
+distros.reverse()
 tasks = [
+    'task-gnome-desktop',
     'task-xfce-desktop',
+    'task-kde-desktop',
     'task-lxde-desktop',
-    'task-lxqt-desktop',
+    'task-gnome-flashback-desktop',
     'task-cinnamon-desktop',
     'task-mate-desktop',
-    'task-gnome-flashback-desktop',
-    'task-gnome-desktop']
+    'task-lxqt-desktop',
+    'task-desktop',
+]
 
 inner_script = f'''
 for task in {' '.join(tasks)}
@@ -32,19 +38,20 @@ done
 '''
 
 for recommends in {True, False}:
+    columns: list[list[int] | list[str]]
     columns = [
         [int(line)
-                  for line in subprocess.check_output(
-                          ['mmdebstrap', distro, '/dev/null', '--quiet',
-                           (f'deb http://archive.debian.org/debian {distro} main'  # EOL
-                            if distro in {'stretch', 'buster', 'bullseye'} else
-                            f'deb http://deb.debian.org/debian {distro} main'),  # not EOL
-                           '--variant=apt', '--dpkgopt=force-unsafe-io',
-                           '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
-                           f'--aptopt=Apt::Install-Recommends "{1 if recommends else 0}"',
-                           f'--customize-hook={inner_script}'],
-                          stderr=subprocess.STDOUT).splitlines()
-                  if line.isdigit()]
+         for line in subprocess.check_output(
+                 ['mmdebstrap', distro, '/dev/null', '--quiet',
+                  (f'deb http://archive.debian.org/debian {distro} main'  # EOL
+                   if distro in {'stretch', 'buster', 'bullseye'} else
+                   f'deb http://deb.debian.org/debian {distro} main'),  # not EOL
+                  '--variant=apt', '--dpkgopt=force-unsafe-io',
+                  '--aptopt=Acquire::http::Proxy "http://localhost:3142"',
+                  f'--aptopt=Apt::Install-Recommends "{1 if recommends else 0}"',
+                  f'--customize-hook={inner_script}'],
+                 stderr=subprocess.STDOUT).splitlines()
+         if line.isdigit()]
         for distro in distros]
     columns.append(tasks)
     rows = [distros + ['task'],
