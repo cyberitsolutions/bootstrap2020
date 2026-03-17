e306be22effc81a6a03d54ef9add3f0c87a793a3
Trent W. Buck <trentbuck@gmail.com>
2026-03-17 20:06:32 +1100

    fixup! (INCOMPLETE) app reviews: completely refactor and overhaul

--- B20/D12/templates/measure-install-footprints/hooks/customize50-measure-install-footprints.py
+++ B20/D12/templates/measure-install-footprints/hooks/customize50-measure-install-footprints.py
@@ -19,4 +19,4 @@ for k in os.environ:
 os.environ['LANG'] = 'en_AU.UTF-8'
 
 subprocess.check_call(
-    ['python3', 'measure-install-footprints.py', args.chroot_path])
+    ['python3', args.chroot_path / 'measure-install-footprints.py', args.chroot_path])
