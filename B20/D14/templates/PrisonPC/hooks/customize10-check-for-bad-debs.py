bc26e2fb2519aff7a3603765359ad832c63b3b48
Trent W. Buck <trentbuck@gmail.com>
2026-04-15 18:45:30 +1000

    squash! fixup! fixup! (INCOMPLETE) D14: apt_preferences (not prisonpc-ersatz Conflicts) for bulky default-deny lists
    Actually outsource the low-risk very-bulky lists to a tool that runs AFTER install.
    This will let the high-risk high-impact shitlists run BEFORE install, nice and fast.

---
+++ B20/D14/templates/PrisonPC/hooks/customize10-check-for-bad-debs.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+""" abort if low-change-medium-impact packages were installed
+
+Shitlisting *-dev (25,000 packages) &c in apt_preferences(5) or Conflicts is too slow.
+In apt_preferences it adds 10s to EVERY apt command (even "apt show mg").
+In Conflicts it takes 600s every time we rebuild the prisonpc-ersatz.
+
+There are a small number of packages we're LIKELY to accidentally install.
+There are a very large number of packages we're unlikely to accidentally install.
+
+Do the former in prisonpc-ersatz, pre-install.
+Do the latter here, post-install.
+This keeps the former fast and the latter comprehensive.
+"""
+
+import argparse
+import logging
+import os
+import pathlib
+import re
+import subprocess
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('chroot_path', type=pathlib.Path)
+args = parser.parse_args()
+os.environ['APT_CONFIG'] = os.environ['MMDEBSTRAP_APT_CONFIG']  # replaces "chroot $1" for apt
+os.environ['DPKG_ROOT'] = str(args.chroot_path)  # replaces "chroot $1" for dpkg
+
+stdout = subprocess.check_output(
+    ['dpkg-query', '--show', '--showformat=${Section}\t${Source}\t${Package}\n'],
+    text=True)
+shit_patterns = set(
+r"""
+(.+/)?shells	.*	.+
+(.+/)?editors	.*	.+
+(.+/)?comm	.*	.+
+(.+/)?database	.*	.+
+(.+/)?debug	.*	.+
+(.+/)?devel	.*	.+
+(.+/)?libdevel	.*	.+
+(.+/)?embedded	.*	.+
+(.+/)?hamradio	.*	.+
+(.+/)?httpd	.*	.+
+(.+/)?news	.*	.+
+(.+/)?oldlibs	.*	.+
+(.+/)?otherosfs	.*	.+
+(.+/)?php	.*	.+
+(.+/)?tex	.*	.+
+(.+/)?zope	.*	.+
+.+	.*	.+-(dev|devel)
+.+	.*	.+-server
+.+	.*	libghc-.+-doc
+.+	.*	.+-(dbg|dbgsym|prof|src|source|dkms|debug|compiler)
+# We ban zip because zip files support AES strong crypto.
+# Therefore we must ban all R packages as well, due to
+# r-* → r-base-core → zip
+(.+/)?gnu-r	.*	.+
+.+	.*	r-.+
+.+	r-.*	+
+""".strip().splitlines())
+
+# good patterns trump shit patterns
+good_patterns = set(
+r"""
+shells		bash
+shells		dash
+editors	libreoffice	.+
+net	openssh	openssh-server
+net	openssh	openssh-sftp-server
+# singularity → python3-numpy → python3-numpy-dev
+python	numpy	python3-numpy-dev
+""".strip().splitlines())
+
+shit_matches = [
+    line
+    for line in stdout.splitlines()
+    if any(re.fullmatch(p, line) for p in shit_patterns if p)
+    if not any(re.fullmatch(p, line) for p in good_patterns if p)]
+if shit_matches:
+    print('Suspicious packages installed!')
+    print(*shit_matches, sep='\n')
+    exit(1)
