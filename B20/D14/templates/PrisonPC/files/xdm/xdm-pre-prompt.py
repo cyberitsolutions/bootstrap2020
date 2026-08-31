440708a237b9578383fe249544de127d8bcc22b3
Trent W. Buck <trentbuck@gmail.com>
2026-08-31 20:08:36 +1000

    D14: do "plymouth quit" in xdm (not systemd)

--- B20/D14/templates/PrisonPC/files/xdm/xdm-pre-prompt.py
+++ B20/D14/templates/PrisonPC/files/xdm/xdm-pre-prompt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+import socket
 import subprocess
 
 __doc__ == """
@@ -9,6 +10,24 @@ but before the xdm login prompt is displayed.
 FIXME: error messages from this script don't end up in journal??
 (Change xdm logfile to /dev/stderr or something, in xdm/Xresources ???)
 """
+
+# reimplement "plymouth --quit"
+# This lets me to ship plymouth in the initrd, but
+# remove it (and all its dependencies) from the rootfs.
+# Copy-paste-edit upstream's plymouth-quit.service to run this.
+# Must run as root -- plymouthd checks.
+# UPDATE: in Debian 14, this was causing plymouth to segfault (visible to end user!)
+#         With plymouth removed from the rootfs AND our minimal plymouth-quit.service removed,
+#         plymouth to xdm handoff still looked fine to the user.
+#         The only minor problem is/was plymouthd process still running in the background,
+#         using a little RAM.  So rather than remove plymouth-quit entirely, try doing it in xdm.
+#         --twb, August 2026
+try:
+    with socket.socket(family=socket.AF_UNIX, type=socket.SOCK_STREAM) as sock:
+        sock.connect(b'\0/org/freedesktop/plymouthd')
+        sock.send(b'Q\0')
+except ConnectionRefusedError:
+    print('<6>plymouth not running - nothing to quit')
 
 
 # Xorg includes a "screen saver", but
